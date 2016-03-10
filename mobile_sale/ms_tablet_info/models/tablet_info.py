a154e7cb57cccaf0fc4814a32111181194886375
System <devuser7th@7thcomputing.com>
2016-03-10 19:05:34 +0630

    MDG-59 Table Information group by Tablet ID

--- mobile_sale/ms_tablet_info/models/tablet_info.py
+++ mobile_sale/ms_tablet_info/models/tablet_info.py
@@ -1,4 +1,5 @@
 from openerp.osv import fields, osv
+from openerp import SUPERUSER_ID
 class tablet_information(osv.osv):
     _name = "tablets.information"
     _description = "Tablet Informations"
@@ -17,4 +18,37 @@ class tablet_information(osv.osv):
     _defaults = {
                
     }  
+    
+    def name_get(self, cr, user, ids, context=None):
+        if context is None:
+            context = {}
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+        if not len(ids):
+            return []
+
+        def _name_get(d):
+            name = d.get('name', '')
+            mac_address = context.get('mac_address', True) and d.get('mac_address', False) or False
+            if mac_address:
+                name = '%s' % (mac_address)
+            return (d['id'], name)
+
+        # all user don't have access to seller and partner
+        # check access and use superuser
+        self.check_access_rights(cr, user, "read")
+        self.check_access_rule(cr, user, ids, "read", context=context)
+
+        result = []
+        for val in self.browse(cr, SUPERUSER_ID, ids, context=context):
+           if val:
+               name = "%s" % (val.mac_address) or val.name
+
+           mydict = {
+                      'id': val.id,
+                      'name': name,
+                      }
+          # print' mydict',mydict
+           result.append(_name_get(mydict))
+        return result
 tablet_information()
