ed82e845045835c77013edcfcfa6dfd10ace739f
System <devuser7th@7thcomputing.com>
2016-07-07 03:51:49 +0630

    MDG-199 Multiple Invoice

--- sale/sale_management/__openerp__.py
+++ sale/sale_management/__openerp__.py
@@ -32,24 +32,23 @@ Sale Management
 """,
     'author': 'Seventh Computing Developer Group',
     'website': 'http://www.7thcomputing.com',
-    'depends': ['base','crm','sale','crm_sale_channel','crm_demarcation','account','account_accountant',
-                'stock','sale_stock','crm_management','ms_sale_plan_setting'
+    'depends': ['base', 'crm', 'sale', 'crm_sale_channel', 'crm_demarcation', 'account', 'account_accountant',
+                'stock', 'sale_stock', 'crm_management', 'ms_sale_plan_setting'
     ],
     'data': [
 
        # 'views/product_product_view.xml',
+       'views/custom_layouts.xml',
+        'views/report_saleorder.xml',
+         'views/report_invoice.xml',
         'views/sale_team_view.xml',
         'views/sale_order_view.xml',
         'views/sale_return_view.xml',
         'views/sale_display_view.xml',
-		'views/report_saleorder.xml',
-         'views/report_invoice.xml',
 		'views/sales_retal_view.xml',
 		'views/sales_denomation_view.xml',
 		'views/sale_approvel_view.xml',
-		'views/code_sequence.xml',
-		'views/sale_promotion_monthly_view.xml'
-
+        'views/account_invoice_view.xml',
     ],
     'installable': True,
     'auto_install': False,
--- sale/sale_management/models/__init__.py
+++ sale/sale_management/models/__init__.py
@@ -1,4 +1,4 @@
-#import product_product
+# import product_product
 import sale_team
 import sale_order
 import sale_return
@@ -6,4 +6,4 @@ import sale_display
 import sales_retal
 import sales_denomation
 import sale_approval
-import sale_promotion_monthly
+import account_invoice
