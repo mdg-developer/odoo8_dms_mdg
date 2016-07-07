661a590955617581e4cd99efc0ddb5af25bd82b2
devuser7th <devuser@7thcomputing.com>
2016-07-07 17:19:12 +0630

    KM:invoice function delete

--- sale/sale_management/models/sale_order.py
+++ sale/sale_management/models/sale_order.py
@@ -399,47 +399,7 @@ class sale_order(osv.osv):
 #         }       
 #         return invoice_vals
 
-    def _prepare_invoice(self, cr, uid, order, lines, context=None):
 
-        """Prepare the dict of values to create the new invoice for a
-           sales order. This method may be overridden to implement custom
-           invoice generation (making sure to call super() to establish
-           a clean extension chain).
-           :param browse_record order: sale.order record to invoice
-           :param list(int) line: list of invoice line IDs that must be attached to the invoice
-           :return: dict of value to create() the invoice
-        """
-        if context is None:
-            context = {}
-        journal_ids = self.pool.get('account.journal').search(cr, uid,
-            [('type', '=', 'sale'), ('company_id', '=', order.company_id.id)],
-            limit=1)
-        if not journal_ids:
-            raise osv.except_osv(_('Error!'),
-                _('Please define sales journal for this company: "%s" (id:%d).') % (order.company_id.name, order.company_id.id))
-        invoice_vals = {
-            'name': order.client_order_ref or '',
-            'origin': order.name,
-            'type': 'out_invoice',
-            'reference': order.client_order_ref or order.name,
-            'account_id': order.partner_id.property_account_receivable.id,
-            'partner_id': order.partner_invoice_id.id,
-            'journal_id': journal_ids[0],
-            'invoice_line': [(6, 0, lines)],
-            'currency_id': order.pricelist_id.currency_id.id,
-            'comment': order.note,
-            'payment_term': order.payment_term and order.payment_term.id or False,
-            'fiscal_position': order.fiscal_position.id or order.partner_id.property_account_position.id,
-            'date_invoice': context.get('date_invoice', False),
-            'company_id': order.company_id.id,
-            'user_id': order.user_id and order.user_id.id or False,
-            'section_id' : order.section_id.id,
-            'tb_ref_no':order.tb_ref_no,
-
-        }
-        # Care for deprecated _inv_get() hook - FIXME: to be removed after 6.1
-        invoice_vals.update(self._inv_get(cr, uid, order, context=context))
-        return invoice_vals    
 
 sale_order()   
 
