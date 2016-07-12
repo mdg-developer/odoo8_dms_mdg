392918902f919424a452590977479a297c6dc706
System <devuser7th@7thcomputing.com>
2016-07-12 22:03:02 +0630

    MDG-215 To Do Lists

--- crm_sale_channel/models/sale_channel.py
+++ crm_sale_channel/models/sale_channel.py
@@ -9,8 +9,12 @@ class sale_channel(osv.osv):
     _name = 'sale.channel'
     _columns = {              
                 'name': fields.char('Channel Name', required=True),
-                'code':fields.char('Channel Code', required=True)                
+                'code':fields.char('Channel Code', required=True),
+                'active':fields.boolean('Active')      
                }
+    _default = {
+              'active':True
+              }
     _sql_constraints = [('Channel_code_uniq', 'unique(code)',
                                      'Channel Code should not be same to others!')]
 sale_channel()
