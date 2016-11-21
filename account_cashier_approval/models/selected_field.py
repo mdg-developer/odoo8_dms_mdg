fe371d1b09d6d6839a82f4330b0f0a66e283d50e
devuser7th <devuser@7thcomputing.com>
2016-11-21 16:52:35 +0630

    Add selected field and condition

---
+++ account_cashier_approval/models/selected_field.py
@@ -0,0 +1,19 @@
+import itertools
+from lxml import etree
+from openerp.osv import fields, osv
+from openerp import models, fields, api, _
+from openerp.tools import float_compare
+import openerp.addons.decimal_precision as dp
+
+class account_invoice(models.Model):
+    _inherit = "account.invoice"
+    
+    
+    unselected = fields.Boolean(string='Unselected',default=False)   
+    
+class mobile_ar_collection(models.Model):
+    _inherit = "mobile.ar.collection"
+     
+    unselected = fields.Boolean(string='Unselected',default=False)   
+   
+    
