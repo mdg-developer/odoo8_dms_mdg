299059436c4f8d2ce11c9fb8ecdd9bc44969c41e
devuser <devuser@7thcomputing.com>
2017-01-03 18:43:02 +0630

    cash report

--- ms_report/cash_report.sql
+++ ms_report/cash_report.sql
@@ -15,13 +15,13 @@ $BODY$
                   from account_move_line aml,                    
                     account_move
                   WHERE                    
-                    aml.move_id = account_move.id
-                  and aml.journal_id in (select id from account_journal where name=''Cash'')';
+                    aml.move_id = account_move.id                  
+                  and aml.account_id in (select id from account_account where name in (''Cash (MMK)'',''Cash Donation''))';
 
   BEGIN
 
     IF date_from is not null and date_to is not null  THEN 
-	 sql := sql || ' AND aml.period_id=(select id from account_period where special=true and fiscalyear_id in (select id from account_fiscalyear where ''' || date_from || ''' between date_start and date_stop))';
+	 sql := sql || ' AND aml.period_id=(select min(id) from account_period)';
     END IF;  
     IF state_cond = 'posted' THEN
        sql := sql || ' AND account_move.state = $1';
@@ -41,7 +41,7 @@ $BODY$
 -- DROP FUNCTION cash_report(date, date, text);
 
 CREATE OR REPLACE FUNCTION cash_report(IN date_from date, IN date_to date, IN state_cond text)
-  RETURNS TABLE(move_date date, ref character varying, partner character varying, account_type character varying, debit numeric, credit numeric, state character varying, seq integer) AS
+  RETURNS TABLE(move_date date, ref character varying, partner character varying, account_type character varying, debit numeric, credit numeric, state character varying, seq integer, reference_no character varying) AS
 $BODY$
 DECLARE 
            
@@ -49,53 +49,49 @@ DECLARE
 
 	if state_cond='posted' then 
 		RETURN QUERY 		
-			select am.date as move_date,am.name as ref,rp.name as partner,aj.name as account_type,COALESCE(sum(aml.debit),0) debit,0 as credit,am.state,1 as seq
+			select am.date as move_date,am.name as ref,rp.name as partner,aj.name as account_type,COALESCE(sum(aml.debit),0) debit,0.0 as credit,am.state,1 as seq,am.ref as reference_no
 			from account_move_line aml,account_move am,account_period period,res_partner rp,account_journal aj
 			where aml.move_id = am.id
 			and period.id=am.period_id
 			and rp.id=aml.partner_id
-			and aj.id=am.journal_id
-			and aml.journal_id in (select id from account_journal where name='Cash')
-			and am.date between date_from and date_to
-			and special=false	
+			and aj.id=am.journal_id			
+			and am.ref like 'SO%'
+			and am.date between date_from and date_to		
 			and am.state='posted'	
-			group by am.date,am.name,rp.name,aj.name,am.state
+			group by am.date,am.name,rp.name,aj.name,am.state,am.ref
 			union
-			select am.date as move_date,am.name as ref,rp.name as partner,aj.name as account_type,0 as debit,COALESCE(sum(aml.credit),0) credit,am.state,2 as seq
+			select am.date as move_date,am.name as ref,rp.name as partner,aj.name as account_type,0.0 as debit,COALESCE(sum(aml.credit),0) credit,am.state,2 as seq,am.ref as reference_no
 			from account_move_line aml,account_move am,account_period period,res_partner rp,account_journal aj
 			where aml.move_id = am.id
 			and period.id=am.period_id
 			and rp.id=aml.partner_id
-			and aj.id=am.journal_id
-			and aml.journal_id in (select id from account_journal where name='Cash')
-			and am.date between date_from and date_to
-			and special=false	
+			and aj.id=am.journal_id			
+			and am.ref like 'PO%'
+			and am.date between date_from and date_to				
 			and am.state='posted'	
-			group by am.date,am.name,rp.name,aj.name,am.state
+			group by am.date,am.name,rp.name,aj.name,am.state,am.ref
 			order by seq;
 	else
 		RETURN QUERY 		
-			select am.date as move_date,am.name as ref,rp.name as partner,aj.name as account_type,COALESCE(sum(aml.debit),0) debit,0 as credit,am.state,1 as seq
+			select am.date as move_date,am.name as ref,rp.name as partner,aj.name as account_type,COALESCE(sum(aml.debit),0) debit,0.0 as credit,am.state,1 as seq,am.ref as reference_no
 			from account_move_line aml,account_move am,account_period period,res_partner rp,account_journal aj
 			where aml.move_id = am.id
 			and period.id=am.period_id
 			and rp.id=aml.partner_id
-			and aj.id=am.journal_id
-			and aml.journal_id in (select id from account_journal where name='Cash')
-			and am.date between date_from and date_to
-			and special=false			
-			group by am.date,am.name,rp.name,aj.name,am.state
+			and aj.id=am.journal_id			
+			and am.ref like 'SO%'
+			and am.date between date_from and date_to		
+			group by am.date,am.name,rp.name,aj.name,am.state,am.ref
 			union
-			select am.date as move_date,am.name as ref,rp.name as partner,aj.name as account_type,0 as debit,COALESCE(sum(aml.credit),0) credit,am.state,2 as seq
+			select am.date as move_date,am.name as ref,rp.name as partner,aj.name as account_type,0.0 as debit,COALESCE(sum(aml.credit),0) credit,am.state,2 as seq,am.ref as reference_no
 			from account_move_line aml,account_move am,account_period period,res_partner rp,account_journal aj
 			where aml.move_id = am.id
 			and period.id=am.period_id
 			and rp.id=aml.partner_id
-			and aj.id=am.journal_id
-			and aml.journal_id in (select id from account_journal where name='Cash')
-			and am.date between date_from and date_to
-			and special=false
-			group by am.date,am.name,rp.name,aj.name,am.state
+			and aj.id=am.journal_id			
+			and am.ref like 'PO%'
+			and am.date between date_from and date_to			
+			group by am.date,am.name,rp.name,aj.name,am.state,am.ref
 			order by seq;
 	end if;
 	
@@ -120,30 +116,37 @@ $BODY$
   BEGIN
 
 	EXECUTE 'select * from cash_report_opening($1,$2,$3)' USING date_from,date_to,state_cond INTO initial_balance;
-
-	if state_cond='posted' then 
-		select COALESCE(sum(aml.debit-aml.credit),0) into amount
-		from account_move_line aml,account_move am,account_period period,res_partner rp,account_journal aj
-		where aml.move_id = am.id
-		and period.id=am.period_id
-		and rp.id=aml.partner_id
-		and aj.id=am.journal_id
-		and aml.journal_id in (select id from account_journal where name='Cash')
-		and am.date between date_from and date_to
-		and special=false	
-		and am.state='posted';
-	else
-		select COALESCE(sum(aml.debit-aml.credit),0) into amount
-		from account_move_line aml,account_move am,account_period period,res_partner rp,account_journal aj
-		where aml.move_id = am.id
-		and period.id=am.period_id
-		and rp.id=aml.partner_id
-		and aj.id=am.journal_id
-		and aml.journal_id in (select id from account_journal where name='Cash')
-		and am.date between date_from and date_to
-		and special=false;			
-	end if;
+	RAISE NOTICE ' initial_balance (%)', initial_balance;
+	EXECUTE 'select COALESCE(sum(debit-credit),0) from cash_report($1,$2,$3)' USING date_from,date_to,state_cond INTO amount;
+	RAISE NOTICE ' amount (%)', amount;
 	balance=initial_balance+amount;
+
+   return balance;
+  END;
+  $BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+  
+-- Function: calculate_opening_balance(date, date, text)
+
+-- DROP FUNCTION calculate_opening_balance(date, date, text);
+
+CREATE OR REPLACE FUNCTION calculate_opening_balance(date_from date, date_to date, state_cond text)
+  RETURNS numeric AS
+$BODY$
+  DECLARE
+    balance numeric;
+    p_id integer;
+  BEGIN
+
+    select id into p_id from account_period where id=(select min(id) from account_period) and date_from between date_start and date_stop;
+    if p_id is not null then
+	EXECUTE 'select * from cash_report_opening($1,$2,$3)' USING date_from,date_to,state_cond INTO balance;
+    else
+	EXECUTE 'select * from cash_report_closing($1,$2,$3)' USING date_from-1,date_to,state_cond INTO balance;
+    end if;
+
+    RAISE NOTICE 'balance (%)', balance;
 
    return balance;
   END;
