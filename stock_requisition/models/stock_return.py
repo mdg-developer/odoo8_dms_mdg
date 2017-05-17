ee369878565fc0aae934947ddd10b71709576797
devuser7th <devuser@7thcomputing.com>
2017-05-17 13:23:40 +0630

    get Record GIN when GIN state is done.

--- stock_requisition/models/stock_return.py
+++ stock_requisition/models/stock_return.py
@@ -77,7 +77,7 @@ class stock_return(osv.osv):
 #             if note:
 #                 note_id=note[0]
 #            print 'rereturn_date',return_date,sale_team_id
-            note_ids = note_obj.search(cr, uid, [('sale_team_id', '=', sale_team_id), ('issue_date', '=', return_date),('state','!=','cancel')])
+            note_ids = note_obj.search(cr, uid, [('sale_team_id', '=', sale_team_id), ('issue_date', '=', return_date),('state','=','issue')])
             if  note_ids:        
                 cr.execute('select gin.from_location_id as location_id,product_id,big_uom_id,sum(big_issue_quantity) as big_issue_quantity,sum(issue_quantity) as issue_quantity,product_uom  as small_uom_id from good_issue_note gin ,good_issue_note_line  ginl where gin.id = ginl.line_id and gin.id in %s group by product_id,from_location_id,big_uom_id,product_uom', (tuple(note_ids),))
                 p_line = cr.fetchall()            
