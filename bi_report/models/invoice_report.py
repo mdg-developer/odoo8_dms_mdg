552d174a622f77032ca71ae644350db08182debd
devuser <devuser@7thcomputing.com>
2017-06-15 16:47:13 +0630

    MDG-474 - Fixed Sale Amount column at Invoice Analysis Report

--- bi_report/models/invoice_report.py
+++ bi_report/models/invoice_report.py
@@ -19,23 +19,23 @@
 #
 ##############################################################################
 from openerp.osv import fields, osv
-
+from openerp.addons.decimal_precision import decimal_precision as dp
 class account_invoice_report(osv.osv):
     _inherit = 'account.invoice.report'
     _columns = {
             'invoice_no':fields.char('Invoice'),
-        'price_unit': fields.float('Unit Price'),
-        'sale_amount':fields.float('Sale Amount'),
-      'price_subtotal': fields.float('Net Sale Amount'),
-        'discount_amt': fields.float('Discount Amount'),
-        'product_type': fields.char('Product Type'),
+        'price_unit': fields.float('Unit Price', digits_compute= dp.get_precision('Product Price')),
+        'sale_amount':fields.float('Sale Amount', digits_compute= dp.get_precision('Product Price')),
+      'price_subtotal': fields.float('Net Sale Amount', digits_compute= dp.get_precision('Product Price')),
+        'discount_amt': fields.float('Discount Amount', digits_compute= dp.get_precision('Product Price')),
+        'product_type': fields.char('Product Type', digits_compute= dp.get_precision('Product Price')),
     }
     _depends = {
         'account.invoice.line': ['price_unit','price_subtotal','discount_amt'],
     }
 
     def _select(self):
-        return  super(account_invoice_report, self)._select() + ", sub.price_unit as price_unit,(sub.product_qty* sub.price_unit ) as  sale_amount, sub.discount_amt as discount_amt,sub.price_subtotal ,sub.product_type as product_type,sub.number as invoice_no"
+        return  super(account_invoice_report, self)._select() + ", sub.price_unit as price_unit, (sub.price_subtotal + sub.discount_amt) as sale_amount, sub.discount_amt as discount_amt,sub.price_subtotal ,sub.product_type as product_type,sub.number as invoice_no"
 
     def _sub_select(self):
         return  super(account_invoice_report, self)._sub_select() + ", ai.number,ail.price_unit,ail.discount_amt,ail.price_subtotal,pt.type as product_type"
