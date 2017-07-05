23591c96ae9ebc3668e877f844b9849d090ec80e
devuser7th <devuser@7thcomputing.com>
2017-07-05 18:11:10 +0630

    MDG-518:Picking Sr.No Change In Picking Form(Odoo)

--- stock_requisition/models/stock.py
+++ stock_requisition/models/stock.py
@@ -10,3 +10,17 @@ class stock_move(osv.osv):
     _columns = {
                 'manual': fields.boolean('Manual'),        
                }
+    
+class stock_picking(osv.osv):
+    _inherit = 'stock.picking'
+    
+    def create(self, cr, user, vals, context=None):
+        context = context or {}
+        id_code = self.pool.get('ir.sequence').get(cr, user,
+                                                'picking.code') or '/'        #if ('name' not in vals) or (vals.get('name') in ('/', False)):
+            #ptype_id = vals.get('picking_type_id', context.get('default_picking_type_id', False))
+            #sequence_id = self.pool.get('stock.picking.type').browse(cr, user, ptype_id, context=context).sequence_id.id
+        vals['name'] = id_code
+        return super(stock_picking, self).create(cr, user, vals,  context=context)    
+    
+    
