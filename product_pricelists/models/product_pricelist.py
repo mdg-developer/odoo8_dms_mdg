c3775ff2bd21882999567bcc8d94813ada0b46fe
DESKTOP-1RNPL3N\Admin <devuser@7thcomputing.com>
2017-07-11 17:15:52 +0630

    RB-78,RB-84

--- product_pricelists/models/product_pricelist.py
+++ product_pricelists/models/product_pricelist.py
@@ -6,7 +6,7 @@ class product_pricelist(osv.osv):
     _columns = {
                 'main_group_id':fields.many2one('product.maingroup','Main Group'),
                 'branch_id':fields.many2one('res.branch',string='Branch'),
-
+                'sales_channel':fields.many2one('sale.channel', 'Sale Channel'),
                 }
 product_pricelist()
 
