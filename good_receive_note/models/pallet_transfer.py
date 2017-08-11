9f67a392cdae5c8b0f0aeb87a4bfdeb79f7e052a
devuser7th <devuser@7thcomputing.com>
2017-08-11 11:17:46 +0630

    MDG-570:Pallet Transfer Form Print And Destination Location Auto Assign

--- good_receive_note/models/pallet_transfer.py
+++ good_receive_note/models/pallet_transfer.py
@@ -67,14 +67,14 @@ class pallet_transfer(osv.osv):
         return super(pallet_transfer, self).create(cursor, user, vals, context=context)
     
     def reserve(self, cr, uid, ids, context=None):
-        transfer_line_obj = self.pool.get('pallet.transfer.line')       
-        product_obj = self.pool.get('product.product')       
+        transfer_line_obj = self.pool.get('pallet.transfer.line')      
+        product_obj = self.pool.get('product.product')      
         quant_obj = self.pool.get('stock.quant')
-        transfer_line_ids = transfer_line_obj.search(cr, uid, [('line_id', '=', ids[0])], context=context) 
+        transfer_line_ids = transfer_line_obj.search(cr, uid, [('line_id', '=', ids[0])], context=context)
         location_data = []
         for transfer_id in transfer_line_ids:
             transfer_data = transfer_line_obj.browse(cr, uid, transfer_id, context=context)
-            location_id = transfer_data.src_location_id.id 
+            location_id = transfer_data.src_location_id.id
             pallet_id = transfer_data.pallet_id.id
             cr.execute("select product_id from stock_quant where package_id =%s", (pallet_id,))
             product_id = cr.fetchone()
@@ -84,9 +84,9 @@ class pallet_transfer(osv.osv):
                 principle_id = product_data.product_tmpl_id.main_group.id
             else:
                 raise osv.except_osv(_('Warning'),
-                                     _('Please Press Your Pallet'))               
-            cr.execute("""select sl.id from product_product pp ,product_template pt ,stock_location sl 
-            where pp.product_tmpl_id =pt.id 
+                                     _('Please Press Your Pallet'))              
+            cr.execute("""select sl.id from product_product pp ,product_template pt ,stock_location sl
+            where pp.product_tmpl_id =pt.id
             and pt.main_group=sl.maingroup_id
             and sl.location_id=%s
             and pt.main_group = %s
@@ -94,11 +94,15 @@ class pallet_transfer(osv.osv):
             location_ids = cr.fetchall()
             for location in location_ids:
                 location_id = location[0]
-                quant_id = quant_obj.search(cr, uid, [('location_id', '=', location_id), ('qty', '>', 0)], context=context)         
+                quant_id = quant_obj.search(cr, uid, [('location_id', '=', location_id), ('qty', '>', 0)], context=context)        
                 if len(quant_id) == 0:
-                    location_data.append(location_id)       
-            cr.execute("select id from stock_location where id in %s and id not in (select dest_location_id from pallet_transfer_line where line_id= %s) order by row ,layer,room,cell", (tuple(location_data), ids[0],))
-            pallet_location = cr.fetchone()[0]
+                    location_data.append(location_id)    
+            cr.execute("select id from stock_location where id in %s and id not in (select dest_location_id from pallet_transfer_line where line_id= %s  and  dest_location_id is not null) order by row ,layer,room,cell", (tuple(location_data), ids[0],))
+            pallet_location = cr.fetchone()
+            if pallet_location:
+                pallet_location=pallet_location[0]
+            else:
+                pallet_location=None
             transfer_line_obj.write(cr, uid, transfer_id, {'dest_location_id':pallet_location}, context=context)                            
         return self.write(cr, uid, ids, {'state': 'reserve'})
     
