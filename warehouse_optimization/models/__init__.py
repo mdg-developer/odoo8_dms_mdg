a4437f0569d27472a7ae278403f34a57adb5643b
devuser7th <devuser@7thcomputing.com>
2017-08-18 13:49:26 +0630

    Inventory Value (See Admin Only)

--- warehouse_optimization/models/__init__.py
+++ warehouse_optimization/models/__init__.py
@@ -1,4 +1,4 @@
 import product_product
 import stock_quant_package
 import stock_location
-
+import stock_quant
---
+++ warehouse_optimization/models/stock_quant.py
@@ -0,0 +1,6 @@
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+
+class stock_quant(osv.osv):
+
+    _inherit = "stock.quant"    
