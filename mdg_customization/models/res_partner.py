f016105275b876c282e5765bdfe33309ea4c2e33
Devuser <devuser@7thcomputing.com>
2017-08-29 12:19:30 +0630

    RB-170 Add Exclusive boolean, Make Customer and supplier visible,mobile_sale_order_report

--- mdg_customization/models/res_partner.py
+++ mdg_customization/models/res_partner.py
@@ -62,6 +62,7 @@ class res_partner(osv.osv):
          'sat':fields.boolean('SAT'),
          'sun':fields.boolean('SUN'),
      'section_id':fields.many2many('crm.case.section', 'sale_team_customer_rel', 'partner_id', 'sale_team_id', string='Sales Team'),
+        'isexclusive':fields.boolean('Exclusive'),
     }
     _defaults = {
                'start_time':'01',
