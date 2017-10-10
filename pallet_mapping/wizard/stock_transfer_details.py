cc23e1298a20235c786fad437bfb56c8bd747eb3
devuser7th <devuser@7thcomputing.com>
2017-10-10 17:20:19 +0630

    MDG-607:Check Extra Move When Picking Transfer Over Qty

--- pallet_mapping/wizard/stock_transfer_details.py
+++ pallet_mapping/wizard/stock_transfer_details.py
@@ -7,6 +7,7 @@ from openerp.osv import orm
 from openerp.osv import fields, osv
 from openerp.tools.translate import _
 from datetime import datetime
+import openerp.addons.decimal_precision as dp
 
 #class stock_transfer_details(models.TransientModel):
 class stock_transfer_details(osv.osv):
@@ -205,5 +206,54 @@ class stock_transfer_details(osv.osv):
                                         stock_quant_package_obj.write(cr,uid,details.result_package_id.id,{'saleable':True,'strickering_state':'retransfer'},context=context)
                                            
         return self.pool['stock.transfer_details'].wizard_view(cr, uid, ids[0], context)
-                          
+    def default_get(self, cr, uid, fields, context=None):
+        if context is None: context = {}
+        res = super(stock_transfer_details, self).default_get(cr, uid, fields, context=context)
+        picking_ids = context.get('active_ids', [])
+        active_model = context.get('active_model')
+
+        if not picking_ids or len(picking_ids) != 1:
+            # Partial Picking Processing may only be done for one picking at a time
+            return res
+        assert active_model in ('stock.picking'), 'Bad context propagation'
+        picking_id, = picking_ids
+        picking = self.pool.get('stock.picking').browse(cr, uid, picking_id, context=context)
+        items = []
+        packs = []
+        if not picking.pack_operation_ids:
+            picking.do_prepare_partial()
+        for op in picking.pack_operation_ids:
+            item = {
+                'packop_id': op.id,
+                'product_id': op.product_id.id,
+                'product_uom_id': op.product_uom_id.id,
+                'quantity': op.product_qty,
+                'temp_qty':op.product_qty,
+                'package_id': op.package_id.id,
+                'lot_id': op.lot_id.id,
+                'sourceloc_id': op.location_id.id,
+                'destinationloc_id': op.location_dest_id.id,
+                'result_package_id': op.result_package_id.id,
+                'date': op.date, 
+                'owner_id': op.owner_id.id,
+            }
+            if op.product_id:
+                items.append(item)
+            elif op.package_id:
+                packs.append(item)
+        res.update(item_ids=items)
+        res.update(packop_ids=packs)
+        return res
+              
+class stock_transfer_details_items(osv.osv):
+    
+    _inherit = 'stock.transfer_details_items'
+    _columns = {
+        'temp_qty': fields.float('Temp Qty', digits=dp.get_precision('temp_qty'), default = 0.0)
+    }
+
+    def on_change_qty_change(self, cr, uid, ids, quantity, temp_qty, context=None):
+        if quantity:
+            if quantity > temp_qty:
+                raise osv.except_osv(('Warning'), ('Quantity should not be larger than available quantity!'))                          
         
