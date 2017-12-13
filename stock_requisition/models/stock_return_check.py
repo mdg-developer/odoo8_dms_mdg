8906150d9436d1f04340f98e9bca572257242fff
unknown <you@example.com>
2017-12-13 15:04:20 +0630

    Add branch id in stock return check form

--- stock_requisition/models/stock_return_check.py
+++ stock_requisition/models/stock_return_check.py
@@ -21,12 +21,7 @@ class stock_return_check(osv.osv):
     _name = "stock.return.check"
     _description = "Stock Return Check"
     _order = "id desc"    
-    
-    _defaults = {
-                 'state' : 'draft',
-                 #'branch_id': _get_default_branch
-    }   
-    
+     
     def on_change_stock_return(self, cr, uid, ids, srn_id, context=None):
         values = {}
         data_line = []
@@ -34,27 +29,27 @@ class stock_return_check(osv.osv):
         if srn_id:
             cr.execute('select srnl.product_id,srnl.product_uom,srnl.actual_return_quantity,srnl.to_location,srnl.status,srn.return_date from stock_return srn, stock_return_line srnl where srn.id=srnl.line_id and srnl.status != %s and srn.id = %s', ('Stock Return',srn_id,))
             stock_return = cr.fetchall()
-            for srn_data_line in stock_return:
-                product_id = srn_data_line[0]
-                pro_data = product_obj.browse(cr, uid, product_id, context=context)
-                sequence=pro_data.sequence
-                uom_id = srn_data_line[1]
-                qty = srn_data_line[2]
-                from_location = srn_data_line[3]
-                status = srn_data_line[4]
-                return_date = srn_data_line[5]
+            if stock_return:
+                for srn_data_line in stock_return:
+                    product_id = srn_data_line[0]
+                    pro_data = product_obj.browse(cr, uid, product_id, context=context)
+                    sequence=pro_data.sequence
+                    uom_id = srn_data_line[1]
+                    qty = srn_data_line[2]
+                    current_location = srn_data_line[3]
+                    status = srn_data_line[4]
+                    return_date = srn_data_line[5]
                 
-                if pro_data.product_tmpl_id.type=='product':
-                    data_line.append({
-                                        'sequence':sequence,
-                                        'product_id':product_id,
-                                        'current_product_uom': pro_data.product_tmpl_id.uom_id and pro_data.product_tmpl_id.uom_id.id or False,
-                                        'current_location':from_location,
-                                        'current_qty':qty,
-                                          })
+                    if pro_data.product_tmpl_id.type=='product':
+                        data_line.append({
+                                          'sequence':sequence,
+                                          'product_id':product_id,
+                                          'current_product_uom': pro_data.product_tmpl_id.uom_id and pro_data.product_tmpl_id.uom_id.id or False,
+                                          'current_location':current_location,
+                                          'current_qty':qty,
+                                            })
                 
             values = {
-                        'return_date':return_date,
                         's_line': data_line,
                     }
         return {'value': values}
@@ -64,42 +59,11 @@ class stock_return_check(osv.osv):
         if not branch_id:
             raise osv.except_osv(_('Error!'), _('There is no default branch for the current user!'))
         return branch_id
-    
-    def create(self, cursor, user, vals, context=None):
-        data_line = []
-        if vals['srn_id']:
-            ref_id=vals['srn_id']
-            srn_data = self.pool.get('stock.return')
-            product_obj = self.pool.get('product.product')
-            if ref_id:
-                cursor.execute('select srnl.product_id,srnl.product_uom,srnl.actual_return_quantity,srnl.to_location,srnl.status,srn.return_date from stock_return srn, stock_return_line srnl where srn.id=srnl.line_id and srnl.status != %s and srn.id = %s', ('Stock Return',ref_id,))
-                stock_return = cursor.fetchall()
-                for srn_data_line in stock_return:
-                    product_id = srn_data_line[0]
-                    pro_data = product_obj.browse(cursor, user, product_id, context=context)
-                    sequence=pro_data.sequence
-                    uom_id = srn_data_line[1]
-                    qty = srn_data_line[2]
-                    from_location = srn_data_line[3]
-            
-                    if pro_data.product_tmpl_id.type=='product':
-                        data_line.append({
-                                        'sequence':sequence,
-                                        'product_id':product_id,
-                                        'current_product_uom': pro_data.product_tmpl_id.uom_id and pro_data.product_tmpl_id.uom_id.id or False,
-                                        'current_location':from_location,
-                                        'current_qty':qty,
-                                          })
-                
-                values = {
-                        's_line': data_line,
-                        }
-        return super(stock_return_check, self).create(cursor, user, vals, context=context)
-    
+        
     _columns = {
-        #'sale_team_id':fields.many2one('crm.case.section', 'Sales Team' , required=True),
+        'sale_team_id':fields.many2one('crm.case.section', 'Sales Team' , required=True),
         'srn_id':fields.many2one('stock.return', 'SRN Ref' , required=True),
-        #'branch_id':fields.many2one('res.branch', 'Branch'),
+        'branch_id':fields.many2one('res.branch', 'Branch',required=True),
         'return_date':fields.date('Date of Return'),
         'state': fields.selection([
             ('draft', 'Pending'),
@@ -110,7 +74,45 @@ class stock_return_check(osv.osv):
               in the invoice validation (Invoice Exception) or in the picking list process (Shipping Exception).\nThe 'Waiting Schedule' status is set when the invoice is confirmed\
                but waiting for the scheduler to run on the order date.", select=True),
         's_line':fields.one2many('stock.return.check.line', 'line_id', 'Product Lines',copy=True),
-}         
+}
+    
+    _defaults = {
+        'state' : 'draft',
+        'branch_id': _get_default_branch
+        }
+    def create(self, cursor, user, vals, context=None):
+        data_line = []
+        if vals['srn_id']:
+            ref_id=vals['srn_id']
+            srn_data = self.pool.get('stock.return')
+            product_obj = self.pool.get('product.product')
+            if ref_id:
+                cursor.execute('select srnl.product_id,srnl.product_uom,srnl.actual_return_quantity,srnl.to_location,srnl.status,srn.return_date from stock_return srn, stock_return_line srnl where srn.id=srnl.line_id and srnl.status != %s and srn.id = %s', ('Stock Return',ref_id,))
+                stock_return = cursor.fetchall()
+                if stock_return:
+                    for srn_data_line in stock_return:
+                        product_id = srn_data_line[0]
+                        pro_data = product_obj.browse(cursor, user, product_id, context=context)
+                        sequence=pro_data.sequence
+                        uom_id = srn_data_line[1]
+                        qty = srn_data_line[2]
+                        current_location = srn_data_line[3]
+                        status = srn_data_line[4]
+                        return_date = srn_data_line[5]
+            
+                        if pro_data.product_tmpl_id.type=='product':
+                            data_line.append({
+                                              'sequence':sequence,
+                                              'product_id':product_id,
+                                              'current_product_uom': pro_data.product_tmpl_id.uom_id and pro_data.product_tmpl_id.uom_id.id or False,
+                                              'current_location':current_location,
+                                              'current_qty':qty,
+                                              })
+                
+                values = {
+                          's_line': data_line,
+                        }
+        return super(stock_return_check, self).create(cursor, user, vals, context=context)
 
 class stock_return_check_line(osv.osv):
     _name = 'stock.return.check.line'
