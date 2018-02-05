352d0b0283229cd2989ded01fad22ab393ddfb20
devuser7th <devuser@7thcomputing.com>
2018-02-05 12:25:29 +0630

    Remove Warehouse Addons

--- stock_quant_packages_moving_wizard/wizard/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-from . import quant_move_wizard
-from . import quants_move_wizard
-from . import quant_packages_move_wizard
