76baa12db0c67db70049dbea335efbd3308c9aba
DESKTOP-KUUC0MD\User <devuser@7thcomputing.com>
2018-03-12 18:07:47 +0630

    MDG-705 fixed supplier refund error

--- sale_journal_principle/account_invoice.py
+++ sale_journal_principle/account_invoice.py
@@ -473,7 +473,7 @@ class account_invoice(models.Model):
                 print 'product>>>', product.id
                 print 'line.get>>>', line.get('product_id', False)
                 if origin and line['is_discount'] == False:
-                    cr.execute("select avl.discount_amt from account_invoice av,account_invoice_line avl  where av.id=avl.invoice_id and av.origin=%s and avl.product_id=%s and av.state!='cancel' ", (origin, product.id,))
+                    cr.execute("select  coalesce(avl.discount_amt, 0) from account_invoice av,account_invoice_line avl  where av.id=avl.invoice_id and av.origin=%s and avl.product_id=%s and av.state!='cancel' ", (origin, product.id,))
                     dis_amt = cr.fetchall()
                     if dis_amt:     
                         for amt in dis_amt:
