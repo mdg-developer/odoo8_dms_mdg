951fcf515a774664785e1e2d6aafb9031ea7dec0
devuser <devuser@7thcomputing.com>
2018-06-08 15:10:08 +0630

    MDG-805:Change Discount Amt Column in report

--- ms_report/sale_promotion_script.sql
+++ ms_report/sale_promotion_script.sql
@@ -151,47 +151,79 @@ ALTER TABLE pre_directsale_with_promotion
 
 -- DROP VIEW promotion_sale_temp_report;
 
-CREATE OR REPLACE VIEW promotion_sale_temp_report AS 
- SELECT DISTINCT (aa.code::text || '-'::text) || aa.name::text AS account_name,
-    aa.name AS account_desc,
-    partner.customer_code AS customer_id,
-    partner.name AS customer,
-    ai.number AS invoice_no,
-    sale_team.name AS sale_team,
-    product.default_code,
-    product.id AS product_id,
-    product.name_template AS product_desc,
-    pt.description_sale,
-    invoice_line.quantity AS qty,
-        CASE
-            WHEN invoice_line.foc = true THEN 'Y'::text
-            ELSE 'N'::text
-        END AS foc,
-    invoice_line.discount_amt,
-    uom.name AS uom,
-    invoice_line.price_unit AS unit_price,
-    invoice_line.price_subtotal AS net_total,
-    invoice_line.discount,
-    invoice_line.price_subtotal::double precision + invoice_line.discount_amt AS sub_total,
-    ( SELECT promos_rules.description
-           FROM promos_rules
-          WHERE promos_rules.id = promodata.pro_id) AS promotion,
-    promodata.pro_id AS promotion_id,
-    ai.branch_id,
-    ai.date_invoice,
-    sale_team.id AS team_id
-   FROM sale_order so,
-    account_invoice ai,
-    account_invoice_line invoice_line,
-    pre_directsale_with_promotion promodata,
-    account_account aa,
-    res_partner partner,
-    crm_case_section sale_team,
-    product_product product,
-    product_uom uom,
-    product_template pt
-  WHERE so.name::text = ai.origin::text AND pt.id = product.product_tmpl_id AND ai.id = invoice_line.invoice_id AND promodata.name::text = so.tb_ref_no::text AND invoice_line.product_id = promodata.product_id AND aa.id = invoice_line.account_id AND so.partner_id = partner.id AND so.section_id = sale_team.id AND invoice_line.product_id = product.id AND invoice_line.uos_id = uom.id
-  ORDER BY ai.number;
+CREATE OR REPLACE VIEW public.promotion_sale_temp_report AS 
+ SELECT a.account_name,
+    a.account_desc,
+    a.customer_id,
+    a.customer,
+    a.invoice_no,
+    a.sale_team,
+    a.default_code,
+    a.product_id,
+    a.product_desc,
+    a.description_sale,
+    a.qty,
+    a.foc,
+    a.discount_amt,
+    a.uom,
+    a.unit_price,
+    a.net_total,
+    a.discount,
+    a.sub_total,
+    a.promotion_des,
+    a.promotion_id,
+    a.branch_id,
+    a.date_invoice,
+    a.team_id,
+    a.payment_type,
+    replace(replace(a.promotion_des::text, '{'::text, ''::text), '}'::text, ''::text) AS promotion
+   FROM ( SELECT DISTINCT (aa.code::text || '-'::text) || aa.name::text AS account_name,
+            aa.name AS account_desc,
+            partner.customer_code AS customer_id,
+            partner.name AS customer,
+            ai.number AS invoice_no,
+            sale_team.name AS sale_team,
+            product.default_code,
+            product.id AS product_id,
+            product.name_template AS product_desc,
+            pt.description_sale,
+            invoice_line.quantity AS qty,
+                CASE
+                    WHEN invoice_line.foc = true THEN 'Y'::text
+                    ELSE 'N'::text
+                END AS foc,
+         --   invoice_line.discount_amt,
+            (CASE WHEN invoice_line.price_subtotal < 0 THEN -1 * invoice_line.price_subtotal ELSE invoice_line.discount_amt END) as discount_amt,
+            uom.name AS uom,
+            --invoice_line.price_unit AS unit_price,
+            (CASE WHEN invoice_line.price_unit < 0 THEN 0 ELSE invoice_line.price_unit END) AS unit_price,
+            invoice_line.price_subtotal AS net_total,
+            invoice_line.discount,
+            invoice_line.price_subtotal::double precision + (CASE WHEN invoice_line.price_subtotal < 0 THEN -1 * invoice_line.price_subtotal ELSE invoice_line.discount_amt END) AS sub_total,
+            ( SELECT array_agg(DISTINCT promos_rules.description) AS array_agg
+                   FROM promos_rules
+                  WHERE promos_rules.id = ANY (array_agg(DISTINCT promodata.pro_id))) AS promotion_des,
+            array_agg(DISTINCT promodata.pro_id) AS promotion_id,
+            ai.branch_id,
+            ai.date_invoice,
+            sale_team.id AS team_id,
+                CASE
+                    WHEN ai.payment_type::text = 'credit'::text THEN 'Credit'::text
+                    ELSE 'Cash'::text
+                END AS payment_type
+           FROM sale_order so,
+            account_invoice ai,
+            account_invoice_line invoice_line,
+            pre_directsale_with_promotion promodata,
+            account_account aa,
+            res_partner partner,
+            crm_case_section sale_team,
+            product_product product,
+            product_uom uom,
+            product_template pt
+          WHERE so.name::text = ai.origin::text AND pt.id = product.product_tmpl_id AND ai.id = invoice_line.invoice_id AND promodata.name::text = so.tb_ref_no::text AND invoice_line.product_id = promodata.product_id AND aa.id = invoice_line.account_id AND so.partner_id = partner.id AND ai.section_id = sale_team.id AND invoice_line.product_id = product.id AND invoice_line.uos_id = uom.id
+          GROUP BY aa.name, aa.code, partner.customer_code, partner.name, ai.number, sale_team.name, product.default_code, pt.description_sale, product.id, invoice_line.quantity, invoice_line.foc, invoice_line.discount_amt, uom.name, invoice_line.price_unit, invoice_line.price_subtotal, invoice_line.discount, ai.branch_id, ai.date_invoice, ai.payment_type, sale_team.id
+          ORDER BY ai.number) a;
 
-ALTER TABLE promotion_sale_temp_report
+ALTER TABLE public.promotion_sale_temp_report
   OWNER TO odoo;
