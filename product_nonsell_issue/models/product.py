c6ddd2744ed36e5b8e225ab0a551a7f47f03e316
devuser <devuser@7thcomputing.com>
2018-06-25 11:45:04 +0630

    MDG-839:Add Destruction Issue Type In Non Sale Issue Form

--- product_nonsell_issue/models/product.py
+++ product_nonsell_issue/models/product.py
@@ -9,6 +9,7 @@ class stock_move(osv.osv):
               'issue_type':fields.selection([
                         ('donation', 'Donation'),
                         ('sampling', 'Sampling'),
+                        ('destruction', 'Destruction'),
                         ('other', 'Others'),
                         ], 'Issue Type'),
                }
@@ -25,7 +26,9 @@ class stock_picking(osv.osv):
               'issue_type':fields.selection([
                         ('donation', 'Donation'),
                         ('sampling', 'Sampling'),
+                        ('destruction', 'Destruction'),
                         ('other', 'Others'),
+                        
                         ], 'Issue Type'),
                }
 class product_nonsell_issue(osv.osv):
@@ -93,6 +96,7 @@ class product_nonsell_issue(osv.osv):
          'issue_type':fields.selection([
             ('donation', 'Donation'),
             ('sampling', 'Sampling'),
+            ('destruction', 'Destruction'),
             ('other', 'Others'),
             ], 'Issue Type'),
           'is_claim': fields.boolean('To Claim'),
@@ -170,7 +174,9 @@ class product_nonsell_issue(osv.osv):
                 if  sell_data.issue_type == 'sampling':
                     from_location_id = location_obj.search(cr, uid, [('name', '=', 'Sampling')], context=context)                
                 if  sell_data.issue_type == 'other':
-                    from_location_id = location_obj.search(cr, uid, [('name', '=', 'Other Uses Location')], context=context)                 
+                    from_location_id = location_obj.search(cr, uid, [('name', '=', 'Other Uses Location')], context=context)       
+                if  sell_data.issue_type == 'destruction':
+                    from_location_id = location_obj.search(cr, uid, [('name', '=', 'Destruction')], context=context)                                    
                 move_id = move_obj.create(cr, uid, {'picking_id': picking_id,
                                        'picking_type_id':picking_type_id,
                                       'product_id': product_id,
@@ -227,7 +233,8 @@ class product_nonsell_issue(osv.osv):
                     account_id = invoice_line.product_id.product_tmpl_id.main_group.property_sampling_account.id           
                 if  issue_type == 'other':
                     account_id = invoice_line.product_id.product_tmpl_id.main_group.property_uses_account.id
-
+                if  issue_type == 'destruction':
+                    account_id = invoice_line.product_id.product_tmpl_id.main_group.property_destruction_account.id
                 inv_line = {'name': invoice_line.product_id.name,
                             'invoice_id':inv_id,
                     'account_id': account_id,
