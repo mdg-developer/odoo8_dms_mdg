2792b03ff0b6988dac44e808734c0c1bc145c2bc
devuser <devuser@7thcomputing.com>
2018-10-16 13:27:00 +0630

    BW-249: Add CTN Remark Column in RFQ

--- purchase_ext/models/purchase_order_line.py
+++ purchase_ext/models/purchase_order_line.py
@@ -14,7 +14,7 @@ class purchase_order_line(osv.osv):
     _inherit = "purchase.order.line"
 
     _columns = {
-        'remark' : fields.char('Remark'),
+        'ctn_remark' : fields.char('CTN'),
         'supplier_code' : fields.char('Supplier Code'),  
         'product_uom': fields.many2one('product.uom', 'Product Unit of Measure', required=True,),
                }
