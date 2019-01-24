159a768e91822e459996936e1b03b5c39d1cf0d5
devuser <devuser@7thcomputing.com>
2019-01-24 13:44:54 +0630

    Warehouse Reports(Daily Closing Stock Check By Date,Location)

---
+++ rb_report/storedprocedure/stock_movement.sql
@@ -0,0 +1,124 @@
+-- Function: stock_movement()
+
+-- DROP FUNCTION stock_movement();
+
+CREATE OR REPLACE FUNCTION stock_movement()
+  RETURNS TABLE(x_id integer, x_date timestamp without time zone, x_location_id integer, x_product_id integer, x_source character varying, x_uom integer, x_from character varying, x_move_in_type character varying, x_transfer_in numeric, x_to character varying, x_move_out_type character varying, x_transfer_out numeric, x_balance numeric) AS
+$BODY$
+BEGIN
+    RETURN QUERY
+select s.id as x_id,
+   s.date as x_date,
+   s.location_id as x_location_id,
+   s.product_id as x_product_id,
+   s.origin as x_source,
+   s.product_uom as x_uom,
+   move_in.team::character varying x_from,
+   move_in.move_type::character varying x_move_in_type,
+   floor(greatest(0,move_in.qty)) x_transfer_in,
+    move_out.team::character varying x_to,
+   move_out.move_type::character varying x_move_out_type,
+   floor(greatest(0,move_out.qty)) x_transfer_out,
+   floor(greatest(0,move_in.qty)) - floor(greatest(0,move_out.qty))
+   as x_balance
+   from
+   (
+       select tmp.id,tmp.location_id,tmp.product_id,tmp.date, tmp.origin, tmp.product_uom
+       from
+       (
+           select s.id,s.location_id,s.product_id,s.date, s.origin ,s.product_uom
+           from stock_move s,
+           stock_location fl
+           where s.state='done'
+           and s.location_id=fl.id
+           and fl.usage!='supplier'
+           and fl.usage!='customer'
+            and fl.usage='internal'
+           union
+           select s.id,s.location_dest_id,s.product_id,s.date, s.origin,s.product_uom
+           from stock_move s,
+           stock_location tl
+           where s.state='done'
+           and s.location_dest_id=tl.id
+           and tl.usage!='supplier'
+           and tl.usage!='customer'
+            and tl.usage='internal'
+       )tmp
+   )
+   s
+
+   left join
+   (
+   select s.id,s.product_id,s.location_dest_id, s.product_qty as qty, s.product_uom,
+--    case when duom.id=suom.id then s.product_qty
+--    when duom.id!=suom.id then (s.product_qty/suom.factor)*duom.factor end as qty, 
+   s.date,split_part(fl.name, '-', 1) team,
+   case when fl.usage='internal' and tl.usage='internal' and tl.scrap_location=false then 'transfer_in'
+   when fl.usage='supplier' and tl.usage='internal' then 'purchase_in'
+   when fl.usage='customer' and tl.usage='internal' then 'sale_return_in'
+   when fl.usage='inventory' and fl.name='Inventory loss' and tl.usage='internal' then 'adjustment+'
+   when fl.usage='internal' and tl.usage='customer' then 'customer warehouse'
+   when fl.usage='internal' and tl.scrap_location=true then 'scrap_bin'
+   when fl.usage='internal' and tl.usage='inventory' and tl.name='Inventory loss' then 'for adjustment- (INV)'
+   end as move_type
+   from stock_move s,
+--    product_product pp,
+--    product_template pt,
+--    product_uom suom,
+--    product_uom duom,
+   stock_location fl,
+   stock_location tl
+   where s.state='done'
+--    and s.product_uom=suom.id
+--    and s.product_id=pp.id
+--    and pp.product_tmpl_id=pt.id
+--    and pt.uom_id=duom.id
+   and s.location_dest_id=tl.id
+   and s.location_id=fl.id
+   and tl.usage!='customer'
+   and tl.usage!='supplier'
+   ) move_in on move_in.product_id=s.product_id and move_in.location_dest_id=s.location_id
+   and move_in.date=s.date and move_in.id=s.id and move_in.product_uom=s.product_uom
+
+   left join
+   (
+   select s.id,s.product_id,s.location_id, s.product_qty as qty,s.product_uom,
+--    case when duom.id=suom.id then s.product_qty
+--    when duom.id!=suom.id then (s.product_qty/suom.factor)*duom.factor end as qty, 
+   s.date,split_part(tl.name, '-', 1) team,
+   case when fl.usage='internal' and tl.usage='internal' and tl.scrap_location=false then 'transfer_out'
+   when fl.usage='internal' and tl.usage='supplier' then 'purchase_return'
+   when fl.usage='internal' and tl.usage='customer' then 'sale_out'
+   when fl.usage='internal' and tl.scrap_location=true then 'scrap'
+   when fl.usage='internal' and tl.usage='inventory' and tl.name='Inventory loss' then 'adjustment-'
+   when fl.usage='supplier' and tl.usage='internal' then 'supplier warehouse'
+   when fl.usage='inventory' and fl.name='Inventory loss' and tl.usage='internal' then 'for adjustment+ (INV)'
+   when fl.usage='customer' and tl.usage='internal' then 'customer_return'
+   end as move_type
+   from stock_move s,
+--    product_product pp,
+--    product_template pt,
+--    product_uom suom,
+--    product_uom duom,
+   stock_location fl,
+   stock_location tl
+   where s.state='done'
+--    and s.product_uom=suom.id
+--    and s.product_id=pp.id
+--    and pp.product_tmpl_id=pt.id
+--    and pt.uom_id=duom.id
+   and s.location_dest_id=tl.id
+   and s.location_id=fl.id
+   and fl.usage!='supplier'
+   and fl.usage!='customer'
+   ) move_out on s.product_id=move_out.product_id and s.location_id=move_out.location_id
+   and move_out.date=s.date and move_out.id=s.id and move_out.product_uom=s.product_uom
+
+   order by s.id,s.location_id,s.product_id,s.date;
+	END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100
+  ROWS 1000;
+ALTER FUNCTION stock_movement()
+  OWNER TO postgres;
