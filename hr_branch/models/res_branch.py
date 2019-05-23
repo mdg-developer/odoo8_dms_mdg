05a72678878152145f3ce185b1ebd9b956b45336
devuser <devuser@7thcmputing.com>
2019-05-23 12:10:44 +0630

    MDG-941::Branch Modification

--- hr_branch/models/res_branch.py
+++ hr_branch/models/res_branch.py
@@ -13,9 +13,38 @@ class branch(osv.osv):
                                         'name': fields.char('Branch Name', required=True),
                     'address': fields.text('Address'),  
                     'active': fields.boolean('Active')  ,
-                    'res_company_id' : fields.many2one('res.company', 'Company')            
+                    'res_company_id' : fields.many2one('res.company', 'Company'),
+                    'branch_location_id' : fields.many2one('stock.location', 'Branch Location'),
+                     #'p_line':fields.one2many('res.branch.line', 'line_id', 'Product Lines',copy=True),
+                    'p_line': fields.one2many('res.branch.line', 'line_id', 'Order Lines'),           
                
                 }
+    
+    def retrieve_data(self, cr, uid, ids, context=None):
+        branch_line_obj = self.pool.get('res.branch.line')
+        cr.execute('delete from res_branch_line where line_id=%s', (ids[0],))
+        cr.execute('''select pp.id,pp.sequence from product_template pt,product_product pp where pt.id=pp.product_tmpl_id
+            and pt.type!='service' and pt.sale_ok=True''')
+        product_data=cr.fetchall()
+        if product_data:                
+            for val in product_data:
+                product_id=val[0]    
+                product_sequence =val[1]   
+                branch_line_obj.create(cr, uid, {'line_id': ids[0],
+                               'sequence':product_sequence,
+                              'product_id': product_id,
+                              'assign':False,
+                        }, context=context)        
+                
+class res_branch_line(osv.osv):
+    _name = "res.branch.line"
+    _columns = {
+                'line_id': fields.many2one('res.branch', 'Line', required=True, ondelete='cascade'),
+                'sequence':fields.integer('Sequence'),         
+                'product_id' : fields.many2one('product.product', 'Product'),
+                'assign' : fields.boolean('Assigned'),
+                }
+    
     _defaults = {
         'active': True,
  
