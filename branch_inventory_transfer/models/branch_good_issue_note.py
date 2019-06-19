ad1b96652bd52a495dfb9669eb91e03ac7d0abc9
zinbomyint <zbm.7thcomputing@gmial.com>
2019-06-19 08:58:50 +0630

    MDG-970::BGIN Form Modification

--- branch_inventory_transfer/models/branch_good_issue_note.py
+++ branch_inventory_transfer/models/branch_good_issue_note.py
@@ -4,6 +4,51 @@ class branch_good_issue_note(osv.osv):
     _inherit = ['mail.thread', 'ir.needaction_mixin']
     _name = "branch.good.issue.note"
     
+    def _get_tcl_value(self, cr, uid, ids, field_name, arg, context=None):
+        res={}
+        tcl_value=0
+        if context is None:
+            context = {}
+            
+        for gin in self.browse(cr, uid, ids, context=context):
+            gin_data = self.browse(cr, uid, gin.id, context=context)
+            if gin_data:
+                if  gin_data.total_value != 0:         
+                    tcl_value = (gin_data.total_tcl / gin_data.total_value)*100
+                                 
+            res[gin_data.id] = tcl_value            
+        return res     
+    
+    
+    def _get_total_tcl(self, cr, uid, ids, field_name, arg, context=None):
+        res={}
+        total_tcl=0.0
+        if context is None:
+            context = {}
+            
+        for gin in self.browse(cr, uid, ids, context=context):
+            gin_data = self.browse(cr, uid, gin.id, context=context)
+            if gin_data:         
+                total_tcl=gin_data.labor_cost + gin_data.transport_cost
+             
+            res[gin_data.id] = total_tcl            
+        return res     
+                      
+        
+    def _get_total_value(self, cr, uid, ids, field_name, arg, context=None):
+        res = {}
+        total_value = 0.0
+        if context is None:
+            context = {}       
+                        
+        for gin in self.browse(cr, uid, ids, context=context):
+            gin_data = self.browse(cr, uid, gin.id, context=context)
+            if gin_data:
+                for line in gin_data.p_line:
+                    total_value = total_value + line.product_value
+            res[gin_data.id] = total_value            
+        return res   
+    
     _columns = {
         'name': fields.char('GIN Ref', readonly=True),
         'branch_id':fields.many2one('res.branch', 'Branch',required=True),
@@ -12,7 +57,7 @@ class branch_good_issue_note(osv.osv):
         'issue_date':fields.date('Date for Issue',required=True),
         'request_id':fields.many2one('stock.requisition', 'RFI Ref', readonly=True),
         'from_location_id':fields.many2one('stock.location', 'Request Warehouse',  readonly=True,required=True),
-        'supplier_id': fields.many2one('res.partner', 'Supplier'),
+        'supplier_id': fields.many2one('res.partner', 'Supplier'), 
         'vehicle_id': fields.many2one('fleet.vehicle', 'Vehicle'),
         'route_id': fields.many2one('transport.route', 'Route'),
         'max_weight': fields.float('Max Weight'),
@@ -26,10 +71,11 @@ class branch_good_issue_note(osv.osv):
         'variable_costing': fields.boolean('Is Variable Costing'),
         'transport_cost': fields.float('Transport Cost'),
         'labor_cost': fields.float('Labor Cost'),
-        'total_tcl': fields.float('Total TCL'),
-        'total_value': fields.float('Total Value'),
+        'total_value': fields.function(_get_total_value, type='float', string='Total Value'),
+        'total_tcl': fields.function(_get_total_tcl, type='float', string='Total TCL'),          
         'transit_location':fields.many2one('stock.location', "Transit Location"),
-        'tcl_value':fields.float("TCL / Value (%)"),
+        'total_tcl': fields.function(_get_total_tcl, type='float', string='Total TCL'),
+        'tcl_value': fields.function(_get_tcl_value, type='float', string='TCL/Value(%)'),
         'p_line':fields.one2many('branch.good.issue.note.line', 'line_id', 'Product Lines',
                               copy=True),
         'state': fields.selection([
@@ -52,13 +98,34 @@ class branch_good_issue_note(osv.osv):
          
     }
     
-    def onchange_total_tcl_calculate(self, cr, uid, ids,transport_cost, labor_cost,total_tcl ,context=None):
-        labor_cost = float(labor_cost)
-        transport_cost= float(transport_cost)
-        total_tcl=float(total_tcl)
-        return {'value' : {'total_tcl' :(labor_cost+transport_cost),}}
- 
+    def create(self, cursor, user, vals, context=None):       
+        id_code = self.pool.get('ir.sequence').get(cursor, user,
+                                                'branch.gin.code') or '/'
+        vals['name'] = id_code       
+        return super(branch_good_issue_note, self).create(cursor, user, vals, context=context)
     
+    def onchange_vehicle(self, cr, uid,ids,vehicle_id,context=None):  
+        if vehicle_id:
+            vehicle = self.pool.get('fleet.vehicle').browse(cr, uid, vehicle_id, context=context)                       
+            route_id = self.pool.get('transport.route.line').search(cr, uid, [('vehicle_id', '=',vehicle_id)], context=context)
+            route_obj= self.pool.get('transport.route.line').browse(cr, uid, route_id, context=context) 
+            if route_id:                    
+                return {'value' : {'max_weight' : vehicle.weight_viss,'max_cbm' : vehicle.vol_cbm,
+                               'supplier_id':vehicle.supplier_id,'transport_cost' :route_obj.transport_cost,'labor_cost' : route_obj.labor_cost,}}
+    
+    def onchange_route(self, cr, uid,ids,route_id,context=None):
+        domain={}
+
+        if route_id:
+            route_ids = self.pool.get('transport.route.line').search(cr, uid, [('line_id', '=',route_id)], context=context)
+            route_list = str(tuple(route_ids))
+            route_list = eval(route_list)
+            cr.execute('SELECT vehicle_id  FROM transport_route_line where id in %s',(route_list,))
+            vehicle_list = cr.fetchall()
+            if vehicle_list:        
+                domain = {'vehicle_id': [('id', 'in', vehicle_list)]}
+        return {'domain':domain}           
+      
 class branch_good_issue_note_line(osv.osv):  
     _name = 'branch.good.issue.note.line'
     
@@ -80,3 +147,4 @@ class branch_good_issue_note_line(osv.osv):
                 ('other_reason', 'Other Reason'),                
             ], 'Remark'),        
     }
+    
