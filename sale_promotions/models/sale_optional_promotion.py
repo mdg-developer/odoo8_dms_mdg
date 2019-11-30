c77f726bf92e55899d91e5e3f25b331140c43eaf
hnin <hnin.7thcomputing@gmail.com>
2019-11-30 13:43:49 +0630

    Optional product in sale promotion

--- sale_promotions/models/sale_optional_promotion.py
+++ sale_promotions/models/sale_optional_promotion.py
@@ -7,17 +7,20 @@ import logging
 from openerp.osv.expression import get_unaccent_wrapper
 from openerp.tools.translate import _
 # from pyfcm import FCMNotification
+
+CONDITION = [    
+    ('>=', _('greater than or equal to')),    
+    ('<=', _('less than or equal to')),
+]
  
 class SaleOptionalPromotion(osv.Model):
-    "Sale Optional Promotion "
+    
     _name = "sale.optional.promotion"
     
-    _columns = {               
-        
-            'product_id': fields.many2one('product.product', 'Product'),
-            'condition': fields.selection([('greater_than', '>'), ('less_than', '<'), ('between', 'Between')], "Condition"),
-            'amount':fields.float('Amount'),
-            'quantity':fields.float('Quantity'),
-            'rule_id':fields.many2one('promos.rules', 'Rule'),
-            'sale_team':fields.many2one('crm.case.section', 'Sale Team'),
+    _columns = {             
+            'product_id': fields.many2one('product.product', 'Product', required=True),
+            'condition': fields.selection(CONDITION, 'Condition', required=True),
+            'amount':fields.float('Amount', required=True),
+            'quantity':fields.float('Quantity', required=True),
+            'rule_id':fields.many2one('promos.rules', 'Rule'),            
         }
