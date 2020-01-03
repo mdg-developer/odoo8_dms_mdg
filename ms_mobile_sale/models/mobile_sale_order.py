61baf4cccfe8ebe1c9557e0694305f932222c85d
hnin <hnin.7thcomputing@gmail.com>
2020-01-03 19:25:38 +0630

    Add get_customers_by_branch function in mobile sale order

--- ms_mobile_sale/models/mobile_sale_order.py
+++ ms_mobile_sale/models/mobile_sale_order.py
@@ -138,6 +138,40 @@ class mobile_sale_order(osv.osv):
         datas = cr.fetchall()
         return datas
     
+    def get_customers_by_branch(self, cr, uid, branch_id , context=None, **kwargs):
+        cr.execute('''
+                    select A.id,A.name,A.image,A.is_company, A.image_small,replace(A.street,',',';') street,replace(A.street2,',',';') street2,A.city,A.website,
+                    replace(A.phone,',',';') phone,A.township,replace(A.mobile,',',';') mobile,A.email,A.company_id,A.customer, 
+                    A.customer_code,A.mobile_customer,A.shop_name ,
+                    A.address,
+                    A.zip,A.state_name,A.partner_latitude,A.partner_longitude,null,A.image_medium,A.credit_limit,
+                    A.credit_allow,A.sales_channel,A.branch_id,A.pricelist_id,A.payment_term_id,A.outlet_type ,
+                    A.city_id,A.township_id,A.country_id,A.state_id,A.unit,A.class_id,A.chiller,A.frequency_id,A.temp_customer,
+                    A.is_consignment,A.hamper,A.is_bank,A.is_cheque
+                    
+                    from (
+                        select RP.id,RP.name,'' as image,RP.is_company,null,
+                        '' as image_small,RP.street,RP.street2,RC.name as city,RP.website,
+                        RP.phone,RT.name as township,RP.mobile,RP.email,RP.company_id,RP.customer, 
+                        RP.customer_code,RP.mobile_customer,OT.name as shop_name,RP.address,RP.zip ,RP.partner_latitude,RP.partner_longitude,RS.name as state_name,
+                        substring(replace(cast(RP.image_medium as text),'/',''),1,5) as image_medium,RP.credit_limit,RP.credit_allow,
+                        RP.sales_channel,RP.branch_id,RP.pricelist_id,RP.payment_term_id,RP.outlet_type,RP.city as city_id,RP.township as township_id,
+                        RP.country_id,RP.state_id,RP.unit,RP.class_id,RP.chiller,RP.frequency_id,RP.temp_customer,RP.is_consignment,RP.hamper,
+                        RP.is_bank,RP.is_cheque
+                        from outlettype_outlettype OT,
+                                             res_partner RP ,res_country_state RS, res_city RC,res_township RT
+                                            where RS.id = RP.state_id
+                                            and RP.township =RT.id
+                                            and RP.city = RC.id
+                                            and RP.active = true                                            
+                                            and RP.outlet_type = OT.id   
+                                            and RP.credit_allow =True                                         
+                                            and RP.branch_id=%s
+                    )A 
+                    where A.customer_code is not null
+            ''', (branch_id,))
+        datas = cr.fetchall()
+        return datas
     
     def get_additional_promos(self, cr, uid , context=None):
         cr.execute('''select id,product_id,amount,rule_id,condition,quantity from sale_optional_promotion''')
