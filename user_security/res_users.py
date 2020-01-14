053347d710f7ebe9e29b4ff09c155718ef7826c2
Khaing Myat <khaingmyat@7thcomputing.com>
2020-01-14 17:07:26 +0630

    Default False in user access

--- user_security/res_users.py
+++ user_security/res_users.py
@@ -30,7 +30,7 @@ class res_users(osv.osv):
         'allow_stock_check':fields.boolean('Allow Stock Check',default=True),
         'allow_rental':fields.boolean('Allow Customer Rental',default=True),
         'allow_feedback':fields.boolean('Allow Customer Feedback',default=True),
-        'allow_customer_create':fields.boolean('Allow Customer Create',default=True),
+        'allow_customer_create':fields.boolean('Allow Customer Create',default=False),
 
 
     }
