3a51b5466ed6804fb2c1d88b056f727b3d9b6ccd
devuser7th <devuser@7thcomputing.com>
2020-01-25 14:58:51 +0630

    MDG -1044: Promotion Syncing To SD and Show the hightlighting Record

--- mdg_sd_sync/models/sale_promotion.py
+++ mdg_sd_sync/models/sale_promotion.py
@@ -6,25 +6,32 @@ import xmlrpclib
 class promos_rules(osv.osv):
     _inherit = 'promos.rules'    
     _columns = {
-        'is_sync_sd':fields.boolean('Is Sync SD'),
+        'is_sync_sd':fields.boolean('Is Sync SD',track_visibility='always'),
              }
     
     def sync_to_sd(self, cr, uid, ids, context=None):
-        url ='http://www.mdgpartnersd.com'
-        db ='subdeal'
-        username = 'admin'
-        password ='MDG@portal2019'
-        common = xmlrpclib.ServerProxy('{}/xmlrpc/2/common'.format(url))
+        
+        sd_uid,url,db,password = self.pool['sd.connection'].get_connection_data(cr, uid, context=None)
+        models = xmlrpclib.ServerProxy('{}/xmlrpc/2/object'.format(url))
         #promos.rules promos_rules
         ret = super(promos_rules, self).copy_data(cr, uid, ids[0], default=None, context=context)
-        sd_uid = common.authenticate(db, username, password, {})
-        models = xmlrpclib.ServerProxy('{}/xmlrpc/2/object'.format(url))
+        data = self.browse(cr,uid,ids[0])
         if ret:
             ret['branch_id'] = [(6, 0, [])]
             ret['sale_channel_id'] = [(6, 0, [])]
-        sdid = models.execute_kw(db, sd_uid, password, 'promos.rules', 'create', [ret]) 
-        #if sdid:
-        #    models.execute_kw(db, sd_uid, password, 'promos.rules', 'write', [sdid],{'branch_id':[(6, 0, [])],'sale_channel_id':[(6, 0, [])]}) 
+        
+        if data.name:
+            promotion_ids = models.execute_kw(db, sd_uid, password,
+                'promos.rules', 'search',
+                [[['name', '=', data.name]]])
+            if promotion_ids:
+                for promotion_id in promotion_ids:
+                    models.execute_kw(db, uid, password, 'promos.rules', 'unlink', [promotion_id])
+                    
+                sdid = models.execute_kw(db, sd_uid, password, 'promos.rules', 'create', [ret])
+            else:        
+                sdid = models.execute_kw(db, sd_uid, password, 'promos.rules', 'create', [ret]) 
+         
         self.write(cr, uid, ids, {'is_sync_sd': True}, context=context)    
           
 promos_rules()   
--- mdg_sd_sync/wizard/__init__.py
+++ mdg_sd_sync/wizard/__init__.py
@@ -1,1 +1,2 @@
 import price_list_multi_sync
+import promotion_rule_multi_sync
