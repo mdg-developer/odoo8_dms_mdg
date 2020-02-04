cf2b88ad0e6f28c40153a200f7ed6486810fc21a
phyo <phyo936@gmail.com>
2020-02-04 07:04:11 +0630

    add sales target branch view added.

--- sale_management/__openerp__.py
+++ sale_management/__openerp__.py
@@ -55,6 +55,7 @@ Sale Management
          'views/sale_promotion_history_view.xml',
         'views/sale_promotion_monthly_view.xml',
         'views/sale_target_view.xml',
+        'views/sale_target_branch_view.xml',
         'reports/qweb_view.xml',
         'reports/report_denomination.xml',
         'reports/custom_layout.xml',
