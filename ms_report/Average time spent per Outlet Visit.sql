d2f1b56463e00d535d7099247d7cf74a74850bb6
Khaing Myat <khaingmyat@7thcomputing.com>
2020-02-29 17:53:42 +0630

    Report SQL

---
+++ ms_report/Average time spent per Outlet Visit.sql
@@ -0,0 +1,24 @@
+﻿CREATE OR REPLACE VIEW public.average_time_spent_per_outlet_visit AS 
+
+select a.*,difference::character varying(64) diff_time,time::character varying(64) original_time from 
+(select((cv.date at time zone 'utc') at time zone 'asia/rangoon')::date as date,
+cast (cv.date::timestamp
+   at time zone 'UTC' at time zone 'asia/rangoon' as time) as time,ti.mac_address tablet_id,
+res.name,ccs.name sale_team,spd.name sale_plan_day,cv.visit_reason reason,rt.name township,
+case when cv.image is null then 'No'
+	 when cv.image is not null  then 'Yes'
+end as image,cv.date -LAG(cv.date)OVER (ORDER BY cv.date ) as difference,
+cv.other_reason remark
+from customer_visit cv
+left join res_partner res on res.id = cv.customer_id 
+left join crm_case_section ccs on  ccs.id = cv.sale_team_id
+left join res_branch rb on rb.id = cv.branch_id
+left join sale_plan_day spd on spd.id = cv.sale_plan_day_id
+left join tablets_information ti on ti.id=cv.tablet_id
+left join res_township rt on rt.id=res.township
+where cv.date::date ='2020-02-29'
+group by ccs.name,cv.date,ti.mac_address,res.name,spd.name,cv.visit_reason,rt.name,cv.image,cv.other_reason
+order by cv.date,ccs.name)a;
+
+ALTER TABLE public.average_time_spent_per_outlet_visit
+  OWNER TO odoo;
---
+++ ms_report/inactive_customer_status.sql
@@ -0,0 +1,29 @@
+﻿
+CREATE OR REPLACE VIEW public.inactive_customer_status AS 
+
+select res.id,customer_code,res.name as customer_name,case when res.active=True then 'True' when res.active=False then 'False' end as active,
+case when res.mobile_customer=True then 'True' when res.mobile_customer=False then 'False' end as mobile_customer,array_to_string(array_agg(distinct rpc.name),', ') AS tags,
+case when res.active=True and mobile_customer=True then 'Pending' 
+when res.active=True and mobile_customer=False then 'Registered' 
+when res.active=False  then 'Inactive' 
+end as customer_status,
+array_to_string(array_agg(distinct ccs.name),', ') AS sales_team,rt.code township,street,display_name,outlet.name outlet
+from res_partner res,sale_team_customer_rel rel,res_township rt,crm_case_section ccs,res_partner_res_partner_category_rel crel,res_partner_category rpc,
+outlettype_outlettype outlet
+where res.id=rel.partner_id 
+and res.township=rt.id
+and rel.sale_team_id=ccs.id
+and rpc.id=crel.category_id
+and res.id=crel.partner_id
+and outlet.id=res.outlet_type
+and res.id not in (
+select partner_id
+from account_invoice 
+where date_invoice between current_date-90 and current_date 
+group by partner_id
+)
+group by res.id,customer_code,res.name,res.active,mobile_customer,res.active,rt.code ,street,display_name,outlet.name
+order by customer_code;
+
+ALTER TABLE public.inactive_customer_status
+  OWNER TO odoo;
