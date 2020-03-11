f1ea4fb01a06416cf2ddd89ade195afe728aca1c
hnin <hnin.7thcomputing@gmail.com>
2020-03-11 11:09:16 +0630

    Add location_group in stock_journal view

--- ms_report/stock_journal.sql
+++ ms_report/stock_journal.sql
@@ -10,6 +10,7 @@ CREATE OR REPLACE VIEW public.stock_journal
     pp.name_template AS product,
     category.name AS product_category,
     region.name,
+	loc_group.name location_group,
     sum(sq.qty) AS small_uom_qty,
     sum(sq.qty) / round(1::numeric / uom.factor)::double precision AS big_uom_qty
    FROM stock_quant sq,
@@ -19,9 +20,11 @@ CREATE OR REPLACE VIEW public.stock_journal
     stock_location loc,
     res_branch branch,
     product_category category,
-    res_branch_region region
+    res_branch_region region,
+	stock_location_group loc_group
   WHERE sq.product_id = pp.id AND pp.product_tmpl_id = pt.id AND pt.report_uom_id = uom.id AND sq.location_id = loc.id AND loc.branch_id = branch.id AND branch.branch_region_id = region.id AND pt.categ_id = category.id
-  GROUP BY pp.name_template, uom.factor, sq.in_date, loc.name, branch.name, category.name, region.name
+  AND loc.stock_location_group_id=loc_group.id
+  GROUP BY pp.name_template, uom.factor, sq.in_date, loc.name, branch.name, category.name, region.name,loc_group.name
   ORDER BY pp.name_template;
 
 ALTER TABLE public.stock_journal
