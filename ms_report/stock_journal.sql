1fe9fe8a36b64f75ef117b87595717c3ba34f6e7
hnin <hnin.7thcomputing@gmail.com>
2020-03-20 21:57:41 +0630

    stock journal view

--- ms_report/stock_journal.sql
+++ ms_report/stock_journal.sql
@@ -9,8 +9,9 @@ CREATE OR REPLACE VIEW public.stock_journal
     loc.name AS stock_location,
     pp.name_template AS product,
     category.name AS product_category,
+    principal.name AS principal,
     region.name,
-	loc_group.name location_group,
+    loc_group.name AS location_group,
     sum(sq.qty) AS small_uom_qty,
     sum(sq.qty) / round(1::numeric / uom.factor)::double precision AS big_uom_qty
    FROM stock_quant sq,
@@ -21,10 +22,10 @@ CREATE OR REPLACE VIEW public.stock_journal
     res_branch branch,
     product_category category,
     res_branch_region region,
-	stock_location_group loc_group
-  WHERE sq.product_id = pp.id AND pp.product_tmpl_id = pt.id AND pt.report_uom_id = uom.id AND sq.location_id = loc.id AND loc.branch_id = branch.id AND branch.branch_region_id = region.id AND pt.categ_id = category.id
-  AND loc.stock_location_group_id=loc_group.id
-  GROUP BY pp.name_template, uom.factor, sq.in_date, loc.name, branch.name, category.name, region.name,loc_group.name
+    product_maingroup principal,
+    stock_location_group loc_group
+  WHERE sq.product_id = pp.id AND pp.product_tmpl_id = pt.id AND pt.report_uom_id = uom.id AND sq.location_id = loc.id AND loc.branch_id = branch.id AND branch.branch_region_id = region.id AND pt.categ_id = category.id AND pt.main_group = principal.id AND loc.stock_location_group_id = loc_group.id
+  GROUP BY pp.name_template, uom.factor, sq.in_date, loc.name, branch.name, category.name, principal.name, region.name, loc_group.name
   ORDER BY pp.name_template;
 
 ALTER TABLE public.stock_journal
