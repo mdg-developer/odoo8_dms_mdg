24c9f13475cbc8d6c1164a288d40fa9fa9db0f9d
PhyoThetPaing <ptp.7thcomputing@gmail.com>
2020-05-19 16:32:15 +0630

    RETAIL-55::Odoo > Add some fields on Customer Form

--- connect_with_ecommerce/models/res_partner.py
+++ connect_with_ecommerce/models/res_partner.py
@@ -6,15 +6,13 @@ class res_partner(osv.osv):
     
     _columns = {  
         'ecom_street': fields.char('Street'),
-        'ecom_street2': fields.char('Street2'),        
-        'ecom_city': fields.char('City'),
+        'ecom_street2': fields.char('Street2'),      
+        'ecom_city_id': fields.many2one('res.city', 'City', ondelete='restrict'),
         'ecom_state_id': fields.many2one("res.country.state", 'State', ondelete='restrict'),
-        'ecom_country_id': fields.many2one('res.country', 'Country', ondelete='restrict'),
-        'ecom_township':fields.many2one('res.township','Township'),
+        'ecom_country_id': fields.many2one('res.country', 'Country', ondelete='restrict'),       
+        'ecom_township_id':fields.many2one('res.township','Township'),
         'ecom_phone': fields.char('Phone'),
-#         'delivery_address': fields.char('Delivery Address'),
-        'ecommerce_phone': fields.char('Phone'),      
-#         'pick_up_location': fields.char('Pick Up Locations'),
-#         'delivery_note': fields.text('Delivery Note'),
+        'ecommerce_phone': fields.char('Phone'),     
+
     }
 
