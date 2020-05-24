0d6530958a5e171ef0d293c43209432a5ce49f1e
hnin <hnin@7thcomputing.com>
2020-05-24 13:19:35 +0630

    rest_api module

---
+++ rest_api/controllers/resources.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from . import auth
+
+from . import model__account_invoice       # need install 'account' module
+from . import model__account_invoice_line  # need install 'account' module
+from . import model__product_template      # need install 'product' module
+from . import model__report
+from . import model__res_partner
+from . import model__sale_order            # need install 'sale' module
+from . import model__sale_order_line       # need install 'sale' module
---
+++ rest_api/models/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from . import rest_api_tokens
