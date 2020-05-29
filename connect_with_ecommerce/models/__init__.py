133bd48958bb99a86795942e4a0c315ef4e82b51
PhyoThetPaing <ptp.7thcomputing@gmail.com>
2020-05-29 14:37:02 +0630

    Remove Ecommerce Page

--- connect_with_ecommerce/models/__init__.py
+++ connect_with_ecommerce/models/__init__.py
@@ -1,7 +1,8 @@
 import res_city
 import product_template
 # import res_country
-import res_partner
+# import res_partner
 import res_township
 import product_pricelist
+# import credit_application
 
