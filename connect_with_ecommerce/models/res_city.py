21f12e11bdba98358b137c4b5e7f8210ac7e8b8d
hnin <hnin@7thcomputing.com>
2020-09-06 21:51:40 +0630

    RETAIL-180::Odoo> out of stock status push

--- connect_with_ecommerce/models/res_city.py
+++ connect_with_ecommerce/models/res_city.py
@@ -8,30 +8,22 @@ class res_city(osv.osv):
                'delivery_team_id':fields.many2one('crm.case.section', 'Delivery Team',required=False),
                }
     
-    def get_stock_status_label(self, cr, uid, ids, city_name=None, township_name=None, product_code=None, context=None):
-
-        city_obj = self.pool.get('res.city')        
-        product_obj = self.pool.get('product.product')
-        if city_name and product_code:
+    def get_stock_status_label(self, cr, uid, ids, city_name=None, township_name=None, context=None):
+         
+        city_obj = self.pool.get('res.city')       
+        if city_name:
             city_data = city_obj.search(cr, uid, [('name', '=', city_name)], context=context)            
             if city_data:
                 city = city_obj.browse(cr, uid, city_data, context=context)
                 if city.delivery_team_id:
-                    issue_location = city.delivery_team_id.issue_location_id.id                    
-                    product_data = product_obj.search(cr, uid, [('default_code', '=', product_code)], context=context)
-                    if product_data:
-                        product = product_obj.browse(cr, uid, product_data, context=context)    
-                        cr.execute('''select COALESCE(sum(qty),0)
-                                    from stock_quant
-                                    where product_id=%s
-                                    and location_id=%s''',(product.id,issue_location,))
-                        quantity = cr.fetchall()                         
-                        if quantity:
-                            if quantity[0][0] > 0:
-                                return 'in stock'
-                            else:
-                                return 'out of stock'
-                    else:
-                        return 'Product is not available'
-                    
+                    issue_location = city.delivery_team_id.issue_location_id.id                             
+                    cr.execute('''select COALESCE(sum(qty),0) qty,default_code product_code
+                                from stock_quant sq,product_product pp,product_template pt
+                                where sq.product_id=pp.id
+                                and pp.product_tmpl_id=pt.id
+                                and location_id=%s
+                                group by default_code''',(issue_location,))
+                    quantity = cr.dictfetchall()                    
+                    if quantity:
+                        return quantity                  
                                     
