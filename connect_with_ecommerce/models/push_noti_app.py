0e170e8d7948937e429b9b3d39dedb50a2d71c88
Hnin <hnin@7thcomputing.com>
2020-11-05 23:06:45 +0630

    RETAIL-384::Odoo > push notification to app when woo API call

--- connect_with_ecommerce/models/push_noti_app.py
+++ connect_with_ecommerce/models/push_noti_app.py
@@ -2,6 +2,7 @@ from openerp.osv import fields, osv
 from openerp.tools.translate import _
 from datetime import timedelta,datetime
 import calendar
+from pyfcm import FCMNotification
 
 class PushNotiApp(osv.osv):
     _name = "push.noti.app"
@@ -14,4 +15,12 @@ class PushNotiApp(osv.osv):
     _defaults = {
         'date': fields.datetime.now,
     } 
-                
+     
+    def create(self, cursor, user, vals, context=None):
+        
+        fcm_api_key = self.pool.get('ir.config_parameter').get_param(cursor, user, 'retail_fcm_api_key', default=False, context=context)
+        if fcm_api_key:
+            push_service = FCMNotification(api_key=fcm_api_key)
+            result = push_service.notify_topic_subscribers(topic_name="promotion_changes", message_body="New promotions are coming up!")
+        return super(PushNotiApp, self).create(cursor, user, vals, context=context)
+    
