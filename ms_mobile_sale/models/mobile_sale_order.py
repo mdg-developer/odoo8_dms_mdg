7da2fdaff4288ab089b2022e867e2cb8cf0ed2ae
Khaing Myat <khaingmyat@7thcomputing.com>
2020-12-12 19:38:48 +0630

    Bug Fix : New Customer Create လုပ်လျှင် Channel  က CONSUMER Channel Default   ဖြစ်နေ

--- ms_mobile_sale/models/mobile_sale_order.py
+++ ms_mobile_sale/models/mobile_sale_order.py
@@ -2907,7 +2907,7 @@ class mobile_sale_order(osv.osv):
 #         datas = cr.fetchall()
 #         return datas
     def get_sale_team_channel(self, cr, uid, sale_team_id , context=None, **kwargs):
-        cr.execute("""select sale_team_id,sale_channel_id from sale_team_channel_rel rel,sale_channel c where rel.sale_team_id =%s  and c.id =rel.sale_channel_id order by c.name asc """, (sale_team_id,))
+        cr.execute("""select sale_team_id,sale_channel_id from sale_team_channel_rel rel,sale_channel c where rel.sale_team_id =%s  and c.id =rel.sale_channel_id order by c.create_date,c.name desc """, (sale_team_id,))
         datas = cr.fetchall()
         return datas
     
