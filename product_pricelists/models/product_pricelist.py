f7aa9ee2dce249a4bf10439979bcc7af87634e2e
Hnin <hnin.7thcomputing@gmail.com>
2021-01-30 20:17:33 +0630

    Add ecommerce related fields

--- product_pricelists/models/product_pricelist.py
+++ product_pricelists/models/product_pricelist.py
@@ -7,6 +7,7 @@ class product_pricelist(osv.osv):
                 'main_group_id':fields.many2one('product.maingroup','Main Group'),
                 'branch_id':fields.many2one('res.branch',string='Branch'),
                 'sales_channel':fields.many2one('sale.channel', 'Sale Channel'),
+                'consumer':fields.boolean('Is Consumer Pricelist' , default=False),
                 }
 product_pricelist()
 
