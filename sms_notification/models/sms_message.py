d990e44ad45a94c427e720df74912be01c5ae23b
Hnin <hnin.7thcomputing@gmail.com>
2021-04-06 18:04:11 +0630

    MDG-1533::Add Retry Function For Fail SMS

--- sms_notification/models/sms_message.py
+++ sms_notification/models/sms_message.py
@@ -72,3 +72,39 @@ class SmsMessage(models.Model):
             vals['errorlog']=error_msg    
                
         return super(SmsMessage, self).create(cursor, user, vals, context=context)
+    
+    def retry_sms(self, cr, uid, context=None):    
+        
+        message_obj = self.pool.get('sms.message').search(cr, uid, [('status', '=', 'fail')])
+        if message_obj:
+            message_data = self.browse(cr, uid, message_obj, context=context)         
+            for message in message_data:       
+                try: 
+                    headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}            
+                    sms_user= 'mdgpro'
+                    password= 'fcdaa533-e23d-4f9e-8480-416454b2dfc2'
+                    url='https://mpg-ids.mytel.com.mm/auth/realms/eis/protocol/openid-connect/token'
+                    payload = {'grant_type': 'client_credentials'}
+                    response = requests.post(url,headers=headers,auth=(sms_user, password), data=payload, verify=False)
+                    if response.status_code == 200:                
+                        content = json.loads(response.content)
+                        token = content['access_token'] 
+                        header = {'Content-Type': 'application/json',
+                                  'Authorization': 'Bearer {0}'.format(token)}                    
+                        sms_url = 'https://mytelapigw.mytel.com.mm/msg-service/v1.3/smsmt/sent'
+                        sms_payload = {
+                                       "source":"MDG",
+                                       "dest":message.phone,
+                                       "content":message.message,
+                                }                 
+                        response = requests.post(sms_url,  json = sms_payload, headers = header,verify=False)                
+                        if response.status_code == 200:
+                            message.write({'status':'success'})                  
+                        else:
+                            message.write({'status':'fail'})            
+                    
+                except Exception as e:         
+                    error_msg = 'Error Message: %s' % (e) 
+                    logging.error(error_msg)
+                    message.write({'status':'fail'})  
+                    message.write({'errorlog':error_msg})                    
