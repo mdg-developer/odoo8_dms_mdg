92547390dbb55d9d50c816a4cbad4672461ad220
devuser7th <devuser@7thcomputing.com>
2021-06-14 17:41:11 +0630

    MDGWH-103

--- mdgcwh_sync/models/branch_stock_requistion.py
+++ mdgcwh_sync/models/branch_stock_requistion.py
@@ -15,7 +15,7 @@ class branch_stock_requisition(osv.osv):
         sd_uid,url,db,password = self.pool['cwh.connection'].get_connection_data(cr, uid, context=None)
         models = xmlrpclib.ServerProxy('{}/xmlrpc/2/object'.format(url))
         so_vals={}
-        so_vals['partner_id'] =1
+        so_vals['partner_id'] =8
                       
         so_vals['order_line']= []   
         if ids:
