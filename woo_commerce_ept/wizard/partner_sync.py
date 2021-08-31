51d1561e47284e3b91814247362030637c929cfb
Hnin <hnin.7thcomputing@gmail.com>
2021-08-31 15:26:47 +0630

    Fixed credit allowed and payment term sync error

--- woo_commerce_ept/wizard/partner_sync.py
+++ woo_commerce_ept/wizard/partner_sync.py
@@ -27,24 +27,23 @@ class partner_sync(osv.osv_memory):
         instance = woo_instance_obj.search(cr, uid, [('state','=','confirmed')], context=context, limit=1)
         if instance:
             woo_instance = self.pool['woo.instance.ept'].browse(cr, uid, instance, context=None)
-            #wcapi = woo_instance.connect_for_point_in_woo() 
             customer_wcapi = woo_instance.connect_for_point_in_woo()                    
         partner_lists=context.get('active_ids', [])
         for partner in self.pool.get('res.partner').browse(cr,uid,partner_lists,context=None):
             #woo_customer_id
-            if customer_wcapi:
+            if customer_wcapi and partner.woo_customer_id:
                 customer_data = {} 
-                if partner.property_payment_term:                                
-                    customer_data = {"payment_term": partner.property_payment_term.name or '',"credit_allowed":partner.credit_allow}
-                     
-                                              
-                    customer_response = customer_wcapi.put('credit-allow/%s'%(partner.woo_customer_id),customer_data)  
+                if partner.property_payment_term:
+                    if partner.credit_allow == True:
+                        credit_allow = 'Yes'
+                    else:
+                        credit_allow = 'No'       
+                    customer_data = credit_allow + ',' + partner.property_payment_term.name                   
+                    woo_customer_id = partner.woo_customer_id.split("_")[1]                    
+                    customer_response = customer_wcapi.put('credit-allow/%s'%(woo_customer_id),customer_data)  
                     if customer_response.status_code not in [200,201]:
                         raise except_orm(_('Error'), _("Error in syncing response customer for woo customer id %s %s") % (partner.woo_customer_id,customer_response.content,)) 
-                     
                 else:
-                    raise except_orm(_('Error'), _("Parnet is not set Payment Term"))
-            
-                #raise except_orm(_('UserError'), _("%s is not ecommerce promotion.You can sync only ecommerce promotion!") % (promo_id.name,))              
+                    raise except_orm(_('Error'), _("Parent is not set Payment Term"))
         return True
     
