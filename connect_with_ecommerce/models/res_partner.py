1c9f65345a9e07d3bfb75cfcb69c1950c93acbde
Hnin <hnin.7thcomputing@gmail.com>
2021-09-28 21:47:16 +0630

    RBB-368::Odoo > Change telenor gateway in sent_opt_code API

--- connect_with_ecommerce/models/res_partner.py
+++ connect_with_ecommerce/models/res_partner.py
@@ -4,6 +4,11 @@ import json
 import logging
 import random
 from datetime import timedelta,datetime
+from oauthlib.oauth2 import MobileApplicationClient
+from requests_oauthlib import OAuth2Session
+from urlparse import urlparse
+from urlparse import parse_qs
+from requests.structures import CaseInsensitiveDict
 
 class res_partner(osv.osv):
     _inherit = 'res.partner'
@@ -37,35 +42,85 @@ class res_partner(osv.osv):
           
     def send_otp_code(self, cr, uid, ids, mobile_phone, context=None):
         
-        if mobile_phone:
-            try: 
-                headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}            
-                sms_user= 'mdgpro'
-                password= 'fcdaa533-e23d-4f9e-8480-416454b2dfc2'
-                url='https://mpg-ids.mytel.com.mm/auth/realms/eis/protocol/openid-connect/token'
-                payload = {'grant_type': 'client_credentials'}
-                response = requests.post(url,headers=headers,auth=(sms_user, password), data=payload, verify=False)
-                number = random.randint(1000,9999)         
-                message = 'Dear customer,your OTP for registration is ' + str(number) + '.Use this code to register your account.'
-                if response.status_code == 200:                
-                    content = json.loads(response.content)
-                    token = content['access_token'] 
-                    header = {'Content-Type': 'application/json',
-                              'Authorization': 'Bearer {0}'.format(token)}                    
-                    sms_url = 'https://mytelapigw.mytel.com.mm/msg-service/v1.3/smsmt/sent'
-                    sms_payload = {
-                                   "source": "RB",
-                                   "dest": mobile_phone,
-                                   "content": message 
-                            }                 
-                    response = requests.post(sms_url,  json = sms_payload, headers = header,verify=False)                
-                    if response.status_code == 200:
-                        return number              
+        if mobile_phone:        
+            data = None     
+            error_msg = None 
+            sms_status = None 
+            try:                   
+                if mobile_phone.startswith('+959'):  
+                    mobile_phone = '959' + str(mobile_phone[4:])              
+                otp = random.randint(1000,9999)                   
+                message = "Dear Valued Customer, " + str(otp) + " is your RB account's OTP code"
+                consumer_key = self.pool.get('ir.config_parameter').get_param(cr, uid, 'telenor_consumer_key')
+                consumer_secret = self.pool.get('ir.config_parameter').get_param(cr, uid, 'telenor_consumer_secret')
+                auth_url = 'https://prod-apigw.mytelenor.com.mm/oauth/v1/userAuthorize?client_id=%s&response_type=code&scope=READ'%(consumer_key)
+                scopes = ['READ']
+                oauth = OAuth2Session(client=MobileApplicationClient(client_id=consumer_key), scope=scopes)
+                authorization_url, state = oauth.authorization_url(auth_url)
+                response = oauth.get(authorization_url)
+                response_url = response.url
+                parsed = urlparse(response_url)
+                code = parse_qs(parsed.query)['code']
+                auth_code = code[0]
+                
+                token_url = "https://prod-apigw.mytelenor.com.mm/oauth/v1/token"
+                token_headers = CaseInsensitiveDict()
+                token_headers["Content-Type"] = "application/x-www-form-urlencoded"
+                token_data = "grant_type=authorization_code&client_id=%s&client_secret=%s&expires_in=86400&code=%s&redirect_uri=https://cms.rbdmyanmar.com/oauth2/callback" %(consumer_key,consumer_secret,auth_code,)
+                resp = requests.post(token_url, headers=token_headers, data=token_data)                  
+                if resp.status_code in [200,201]:                 
+                    result = resp.json()
+                    access_token = result['accessToken']                      
+                sms_url = "https://prod-apigw.mytelenor.com.mm/v3/mm/en/communicationMessage/send"
+                sms_headers = CaseInsensitiveDict()
+                sms_headers["Authorization"] = "Bearer " + access_token
+                sms_headers["Content-Type"] = "application/json"                
+                sms_data = """           
+                {
+                   "type": "TEXT",
+                   "content": "%s",
+                   "characteristic": [                                
+                      {
+                         "name": "UserName",
+                         "value": "B2BRBDCo"
+                      },
+                      {
+                         "name": "Password",
+                         "value": "B2BRBD"
+                      }
+                   ],
+                   "sender": {
+                      "@type": "5",
+                      "name": "RBD MYANMAR"
+                   },
+                   "receiver": [
+                      {
+                         "@type": "1",
+                         "phoneNumber": "%s"
+                      }
+                   ]
+                }
+                """ %(message,mobile_phone,)                
+                response = requests.post(sms_url, headers=sms_headers, data=sms_data)   
+                if response.status_code in [200,201]:  
+                    sms_status = 'success'  
+                    data = otp
             except Exception as e:         
                 error_msg = 'Error Message: %s' % (e) 
-                logging.error(error_msg)  
-                return error_msg
-
+                logging.error(error_msg)
+                sms_status = 'fail'                 
+                data = error_msg
+            if error_msg:
+                error_log = error_msg
+            else:
+                error_log = None
+            cr.execute("SELECT now()::timestamp;")    
+            current_datetime_data = cr.fetchall()
+            for time_data in current_datetime_data:
+                current_datetime = time_data[0]                     
+            cr.execute("""insert into sms_message(name,phone,message,text_message,error_log,status,create_date,create_uid) 
+                        values(%s,%s,%s,%s,%s,%s,%s,%s)""",('RB OTP',mobile_phone,message,message,error_log,sms_status,current_datetime,1,))  
+            return data     
 
     def create_or_update_woo_customer(self, cr, uid, ids, mdg_customer=False, customer_code=None, name=None,street=None,street2=None,township=None,city=None,state=None,mobile=None,phone=None,gender=None,birthday=None,email=None,partner_latitude=None,partner_longitude=None,sms=None,viber=None,shop_name=None,woo_customer_id=None,image=None,sale_channel=None,customer_type=None,woo_user_name=None,context=None):
         vals = {}
