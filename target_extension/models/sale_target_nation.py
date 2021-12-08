8bb3be62a91cf3ef045c785ec08efa3a3440a7c0
Hnin <hnin.7thcomputing@gmail.com>
2021-12-08 23:00:34 +0630

    MDG-1706
    Odoo >>MDGSD:Show alert when product line is duplicate in one form

---
+++ target_extension/models/sale_target_nation.py
@@ -0,0 +1,15 @@
+from openerp import models, fields, api, _
+from openerp.osv.orm import except_orm
+
+class SalesTargetNation(models.Model):    
+    _inherit = "sales.target.nation"
+    
+    @api.one
+    @api.constrains('target_line')
+    def check_target_line(self):
+        
+        for line in self.target_line:
+            target_lines = self.env["sales.target.nation.line"].search([('sale_ids', '=', self.id), 
+                                                                        ('product_id', '=', line.product_id.id)])
+            if target_lines and len(target_lines) > 1:
+                raise except_orm(_('UserError'), _("%s is duplicated!") % (line.product_id.name_template,))
