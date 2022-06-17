0847856fd58c2df11a0a00d3c7162a4659eeb562
Lamin Pyae <devuser@7thcomputing.com>
2022-06-17 09:03:57 +0630

    FW-2:Add some filed in Product Form

--- branch_inventory_transfer/models/product.py
+++ branch_inventory_transfer/models/product.py
@@ -9,5 +9,16 @@ class product_template(osv.osv):
          'viss_value': fields.float('Viss', digits_compute=dp.get_precision('Cost Price')),
          'cbm_value': fields.float('CBM', digits_compute=dp.get_precision('Cost Price')),
          'ctn_pallet': fields.float('Ctn/Pallet'),
+         'principal_description': fields.char('Principal Description'),
+         'supplier_name': fields.many2one('res.partner', 'Supplier Name', domain="['is_supplier','=',True]"),
+         'country': fields.many2one('res.country', 'Country'),
+         'variety': fields.char('Variety'),
+         'vintage': fields.many2one('sale.vintage', 'Vintage')
         }
+
+
+class VintageSale(osv.osv):
+    _name = 'sale.vintage'
+
+    name = fields.char('Vintage Name')
     
