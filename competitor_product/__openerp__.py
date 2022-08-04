504ea59a8a7d73e752d6d39be0636785b60950dd
Hnin <hnin@7thcomputing.com>
2022-08-04 15:27:21 +0630

    MDG-1894::Odoo >>Add Competitor Product Form and List View

---
+++ competitor_product/__openerp__.py
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Competitor Product',
+    'version': '1.0',
+    'category': 'Product',
+    'sequence': 30,
+    'summary': 'Competitor Product',
+    'description': """
+Competitor Product
+
+""",
+    'author': 'Seventh Computing',
+    'website': 'http://www.7thcomputing.com',
+    'depends': ['base','product','sale_group'],
+    'data': [
+       'views/competitor_product_view.xml',
+       'views/sales_group_view.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
