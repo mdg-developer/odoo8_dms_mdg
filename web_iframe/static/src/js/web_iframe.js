e2962cf032d701de8d49697af99cb949f43fe06c
Khaing Myat <khaingmyat@7thcomputing.com>
2022-08-20 23:34:24 +0630

    add team para

--- web_iframe/static/src/js/web_iframe.js
+++ web_iframe/static/src/js/web_iframe.js
@@ -3,6 +3,7 @@ openerp.web_iframe = function (session) {
        _lt = session.web._lt;
 
     var web_iframe = session.web_iframe;
+    var rpc = require('web.rpc');
 
     session.web.client_actions.add('web_iframe.iframe', 'session.web_iframe.iframe');
     web_iframe.iframe = session.web.Widget.extend({
@@ -23,8 +24,20 @@ openerp.web_iframe = function (session) {
             }, this.action.params);
         },
         start: function() {
+        	rpc.query({
+                model: 'res.users',
+                method: 'read',
+                args: [[session.uid], ['section_ids']],
+            }).then(function (record) {
+                if (record)
+                {
+                	var team_url = '';
+                    if (record[0]['section_ids'].length > 0) {
+                    	team_url += '&p.team='+ record[0]['section_ids'];
+                    }
+                };
             this.$el.find('iframe').css({height: '100%', width: '100%', border: 0});
-            this.$el.find('iframe').attr({src: this.action.params.link+'&user_id='+this.session.uid});
+            this.$el.find('iframe').attr({src: this.action.params.link+team_url});
             this.$el.find('iframe').on("load", this.bind_events.bind(this));
             return this._super();
         },
