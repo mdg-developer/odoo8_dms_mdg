0892f2279acd88b3bc6c6390649c005485a2a732
Hnin <hnin@7thcomputing.com>
2022-12-12 23:29:30 +0630

    MDG-1961::Odoo >> Get Point Info API for all users

--- woocommerce_loyalty_points/models/woo_point_history.py
+++ woocommerce_loyalty_points/models/woo_point_history.py
@@ -12,6 +12,7 @@ class woo_point_history(osv.osv):
     _columns = {
         'woo_point_history_id': fields.char('Woo Point History ID'),
         'woo_customer_id': fields.char('Woo Customer ID'),
+        'woo_user_id': fields.char('Woo User ID'),
         'partner_id': fields.many2one('res.partner', 'Customer'),
         'action': fields.char('Action'),
         'order_id': fields.many2one('sale.order', 'Sale Order'),
@@ -26,49 +27,58 @@ class woo_point_history(osv.osv):
 
         partner_obj = self.pool.get('res.partner')
         point_history_obj = self.pool.get('woo.point.history')
-        partners = partner_obj.search(cr, uid, [('woo_customer_id', '!=', False),('parent_id', '=', False)])
-        for data in partners:
-            partner = partner_obj.browse(cr, uid, data)
-
-            woo_instance_obj = self.pool.get('woo.instance.ept')
-            instance = woo_instance_obj.search(cr, uid, [('state', '=', 'confirmed')], limit=1)
-            if instance:
-                instance_obj = woo_instance_obj.browse(cr, uid, instance)
-                if partner.woo_customer_id:
-                    woo_customer_id = partner.woo_customer_id.split("_")[1]
-                    url = instance_obj.host + "/wp-json/auth-api/v1/odoo/users/get-points-info?user_id=" + woo_customer_id
-                    headers = CaseInsensitiveDict()
-                    login_info = instance_obj.admin_username + ":" + instance_obj.admin_password
-                    login_info_bytes = login_info.encode('ascii')
-                    base64_bytes = base64.b64encode(login_info_bytes)
-                    headers["Authorization"] = "Basic " + base64_bytes
-                    headers["Content-Type"] = "application/json"
-                    point_response = requests.get(url, headers=headers)
-                    if point_response.status_code in [200, 201]:
-                        point_history = point_response.json()
-                        total_points = point_history.get('total_points')
-                        partner.write({"total_points": total_points})
-                        point_history = point_history.get('point_history')
-                        for item in point_history:
-                            woo_point_history_id = item.get('id')
-                            woo_point_history = point_history_obj.search(cr, uid, [('woo_point_history_id', '=', woo_point_history_id)])
-                            if not woo_point_history:
-                                if item.get('expire_date') != '0000-00-00 00:00:00':
-                                    expire_date = item.get('expire_date')
-                                else:
-                                    expire_date = None
-                                sale_order = self.pool.get('sale.order').search(cr, uid, [('woo_order_id', '=', int(item.get('order_id')))])
-                                sale_order_obj = self.pool.get('sale.order').browse(cr, uid, sale_order)
-                                values = {
-                                    'woo_point_history_id': woo_point_history_id,
-                                    'woo_customer_id': partner.woo_customer_id,
-                                    'partner_id': partner.id,
-                                    'action': item.get('action'),
-                                    'order_id': sale_order_obj.id,
-                                    'amount': float(item.get('amount')),
-                                    'date_earning': item.get('date_earning'),
-                                    'expire_date': expire_date,
-                                    'title': item.get('title'),
-                                    'custom_order_number': item.get('custom_order_number'),
-                                }
-                                point_history_obj.create(cr, uid, values, context=context)
+        customer_point_obj = self.pool.get('woo.customer.point')
+        woo_instance_obj = self.pool.get('woo.instance.ept')
+        instance = woo_instance_obj.search(cr, uid, [('state', '=', 'confirmed')], limit=1)
+        if instance:
+            instance_obj = woo_instance_obj.browse(cr, uid, instance)
+            url = instance_obj.host + "/wp-json/auth-api/v1/odoo/users/get-points-info"
+            headers = CaseInsensitiveDict()
+            login_info = instance_obj.admin_username + ":" + instance_obj.admin_password
+            login_info_bytes = login_info.encode('ascii')
+            base64_bytes = base64.b64encode(login_info_bytes)
+            headers["Authorization"] = "Basic " + base64_bytes
+            headers["Content-Type"] = "application/json"
+            point_response = requests.get(url, headers=headers)
+            if point_response.status_code in [200, 201]:
+                point_data = point_response.json()
+                cr.execute("delete from woo_customer_point;")
+                for record in point_data:
+                    user_id = record.get('user_id')
+                    total_points = record.get('total_points')
+                    point_history = record.get('point_history')
+                    total_points_data = {
+                        'woo_user_id': str(user_id),
+                        'total_points': float(total_points),
+                    }
+                    customer_point_obj.create(cr, uid, total_points_data, context=context)
+                    for item in point_history:
+                        if item.get('expire_date') != '0000-00-00 00:00:00':
+                            expire_date = item.get('expire_date')
+                        else:
+                            expire_date = None
+                        existing_point_history = point_history_obj.search(cr, uid, [('woo_point_history_id', '=', item.get('id'))], context=context)
+                        if not existing_point_history:
+                            point_history_data = {
+                                'woo_point_history_id': item.get('id'),
+                                'woo_user_id': item.get('user_id'),
+                                'expire_date': expire_date,
+                                'action': item.get('action'),
+                                'amount': item.get('amount'),
+                                'date_earning': item.get('date_earning'),
+                                'title': item.get('title'),
+                                'custom_order_number': item.get('custom_order_number'),
+                            }
+                            point_history_obj.create(cr, uid, point_history_data, context=context)
+                cr.execute("""update woo_point_history
+                            set partner_id=rp.id,woo_customer_id=concat('1_',woo_point_history.woo_user_id)
+                            from res_partner rp
+                            where rp.woo_customer_id=concat('1_',woo_point_history.woo_user_id)
+                            and woo_point_history.partner_id is null
+                            and rp.parent_id is null;""")
+                cr.execute("""update woo_point_history
+                            set order_id=so.id
+                            from sale_order so
+                            where woo_point_history.custom_order_number=so.woo_order_number
+                            and woo_point_history.order_id is null;""")
+                cr.execute("update res_partner set total_points=cp.total_points from woo_customer_point cp where res_partner.woo_customer_id=concat('1_',cp.woo_user_id);")
