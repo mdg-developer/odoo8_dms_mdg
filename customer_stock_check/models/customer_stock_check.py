c4b4c31332a6e028843d16df3e31d96b118290b9
Hnin <devuser@7thcomputing.com>
2023-05-25 23:06:05 +0630

    MDG-2036::Odoo >>Add Expiry Date in Stock Check Form

--- customer_stock_check/models/customer_stock_check.py
+++ customer_stock_check/models/customer_stock_check.py
@@ -99,7 +99,8 @@ class customer_stock_check_line(osv.osv):
                 'facing':fields.float('Facing', default=False),
                 'chiller':fields.float('Chiller Qty'),
                 'remark_id':fields.many2one('partner.stock.check.remark', 'Remark'),      
-                'description':fields.char( 'Description'),                
+                'description':fields.char( 'Description'),
+                'expiry_date':fields.date('Expiry Date'),
                           
                 }
 
