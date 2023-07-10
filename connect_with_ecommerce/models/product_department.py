56d57e5397a2d8f832f3d7bdc8e355e97992418f
SHL MDG <developer@mdgmyanmar.com>
2023-07-10 15:17:04 +0630

    [Feature]In Product Form, MDG Principal and MDG Category relation, name changes and correction.

--- connect_with_ecommerce/models/product_department.py
+++ connect_with_ecommerce/models/product_department.py
@@ -4,6 +4,7 @@ class product_department(osv.osv):
     _name = "product.department"
     _columns = {
         'name': fields.char('Name'),
+        'group_id': fields.many2one('product.group','Burmart Category'),
     }
 
 product_department()
