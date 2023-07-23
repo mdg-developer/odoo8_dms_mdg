3454ed59a1f442784911db13b64a06582eb32265
Hnin <devuser@7thcomputing.com>
2023-07-23 22:05:27 +0630

    Update stock quant scheduler

--- warehouse_optimization/models/stock_quant.py
+++ warehouse_optimization/models/stock_quant.py
@@ -3,4 +3,7 @@ from openerp.tools.translate import _
 
 class stock_quant(osv.osv):
 
-    _inherit = "stock.quant"    
+    _inherit = "stock.quant"
+
+    def update_stock_quant(self, cr, uid, context=None):
+        cr.execute("select * from update_stock_quant()")
